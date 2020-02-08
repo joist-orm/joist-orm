78677c4304297966f54558d3154fe807cfd3be3a
Stephen Haberman <stephen.haberman@gmail.com>
2020-02-08 11:42:05 -0600

    Move join table loader into ManyToManyCollection.

--- src/EntityManager.ts
+++ src/EntityManager.ts
@@ -3,9 +3,8 @@ import Knex from "knex";
 import { flushEntities } from "./EntityPersister";
 import { getOrSet } from "./utils";
 import { OneToManyCollection } from "./collections/OneToManyCollection";
-import { ColumnSerde, keyToNumber, keyToString } from "./serde";
+import { ColumnSerde, keyToString } from "./serde";
 import { Collection, LoadedCollection, LoadedReference, Reference } from "./index";
-import { ManyToManyCollection } from "./collections/ManyToManyCollection";
 
 export interface EntityConstructor<T> {
   new (em: EntityManager, opts?: Partial<T>): T;
@@ -38,10 +37,14 @@ export type Loaded<T extends Entity> = {
   [P in keyof T]: MarkLoaded<T, P>;
 };
 
-export class EntityManager {
-  constructor(private knex: Knex) {}
+export type LoaderCache = Record<string, DataLoader<any, any>>;
 
-  private loaders: Record<string, DataLoader<any, any>> = {};
+export class EntityManager {
+  /// TODO Hide impl
+  constructor(public knex: Knex) {}
+
+  // Make private
+  loaders: LoaderCache = {};
   private entities: Entity[] = [];
 
   async find<T extends Entity>(type: EntityConstructor<T>, where: FilterQuery<T>): Promise<T[]> {
@@ -62,11 +65,6 @@ export class EntityManager {
 
   async loadCollection<T extends Entity, U extends Entity>(collection: OneToManyCollection<T, U>): Promise<U[]> {
     return this.loaderForCollection(collection).load(collection.entity.id);
-  }
-
-  async loadJoinTable<T extends Entity, U extends Entity>(collection: ManyToManyCollection<T, U>): Promise<U[]> {
-    const key = collection.dataLoaderKey();
-    return this.loaderForJoinTable(collection).load(key);
   }
 
   /** Registers a newly-instantiated entity with our EntityManager; only called by entity constructors. */
@@ -149,69 +147,6 @@ export class EntityManager {
     });
   }
 
-  private loaderForJoinTable<T extends Entity, U extends Entity>(collection: ManyToManyCollection<T, U>) {
-    const { joinTableName } = collection;
-    const { em } = collection.entity.__orm;
-    return getOrSet(this.loaders, joinTableName, () => {
-      return new DataLoader<string, Entity[]>(async keys => {
-        // Break out `column_id=string` keys out
-        const columns: Record<string, string[]> = {};
-        keys.forEach(key => {
-          const [columnId, id] = key.split("=");
-          getOrSet(columns, columnId, []).push(id);
-        });
-
-        // Or together `where tag_id in (...)` and `book_id in (...)`
-        let query = this.knex.select("*").from(joinTableName);
-        Object.entries(columns).forEach(([columnId, values]) => {
-          query = query.orWhereIn(
-            columnId,
-            values.map(id => keyToNumber(id)!),
-          );
-        });
-
-        const rows = (await query.orderBy("id")) as JoinRow[];
-
-        // Make a map that will be both `tag_id=2 -> [...]` and `book_id=3 -> [...]`
-        const rowsByKey: Record<string, JoinRow[]> = {};
-
-        // Each join table row might have some promises to load the other entity. Eventually
-        // We should just pull the other side in via the ^ query.
-        const p = rows.map(async row => {
-          // For this join table row, resolve the entities both of foreign keys. For now
-          // we're using the EntityManager.load to do this in batch for this, but eventually
-          // we should pull those into the row itself.
-          const p = Object.entries(row).map(async entry => {
-            const [column, value] = entry;
-            if (column === "id" || column === "created_at") {
-              return;
-            }
-
-            // We'll do this twice per join table row, but in batch.
-            const cstr = (column === collection.columnName
-              ? collection.entity.__orm.metadata.cstr
-              : collection.otherType) as EntityConstructor<any>;
-
-            row[column] = await em.load(cstr, keyToString(value)!);
-
-            // Put this row into the map for both join table columns, i.e. `book_id=2` and `tag_id=3`
-            const key = `${column}=${value}`;
-            getOrSet(rowsByKey, key, []).push(row);
-          });
-          await Promise.all(p);
-        });
-        await Promise.all(p);
-
-        return keys.map(key => {
-          const [column] = key.split("=");
-          const joinRows = rowsByKey[key] || [];
-          const otherColumn = column === collection.columnName ? collection.otherColumnName : collection.columnName;
-          return joinRows.map(joinRow => joinRow[otherColumn] as Entity);
-        });
-      });
-    });
-  }
-
   // Handles our Unit of Work-style look up / deduplication of entity instances.
   private findExistingInstance(type: string, id: string): Entity | undefined {
     return this.entities.find(e => e.__orm.metadata.type === type && e.id === id);
@@ -230,5 +165,3 @@ export interface EntityMetadata<T extends Entity> {
 export function isEntity(e: any): e is Entity {
   return e !== undefined && e instanceof Object && "id" in e && "__orm" in e;
 }
-
-type JoinRow = { id: number; created_at: Date } & { [column: string]: number | Entity };
--- src/collections/ManyToManyCollection.ts
+++ src/collections/ManyToManyCollection.ts
@@ -1,5 +1,9 @@
+import DataLoader from "dataloader";
+import Knex from "knex";
 import { Collection } from "../index";
-import { Entity, EntityConstructor } from "../EntityManager";
+import { Entity, EntityConstructor, LoaderCache } from "../EntityManager";
+import { getOrSet } from "../utils";
+import { keyToNumber, keyToString } from "../serde";
 
 export class ManyToManyCollection<T extends Entity, U extends Entity> implements Collection<T, U> {
   constructor(
@@ -19,16 +23,80 @@ export class ManyToManyCollection<T extends Entity, U extends Entity> implements
 
   async load(): Promise<ReadonlyArray<U>> {
     const em = this.entity.__orm.em;
-    return em.loadJoinTable(this);
+    // TODO This is basically a reference
+    // TODO Unsaved entities should never get here
+    const key = `${this.columnName}=${this.entity.id}`;
+    return loaderForJoinTable(em.knex, em.loaders, this).load(key);
   }
 
   add(other: U): void {}
-
-  // internal impls
-
-  public dataLoaderKey(): string {
-    // TODO This is basically a reference
-    // TODO Unsaved entities should never get here
-    return `${this.columnName}=${this.entity.id}`;
-  }
 }
+
+function loaderForJoinTable<T extends Entity, U extends Entity>(
+  knex: Knex,
+  loaders: LoaderCache,
+  collection: ManyToManyCollection<T, U>,
+) {
+  const { joinTableName } = collection;
+  const { em } = collection.entity.__orm;
+  return getOrSet(loaders, joinTableName, () => {
+    return new DataLoader<string, Entity[]>(async keys => {
+      // Break out `column_id=string` keys out
+      const columns: Record<string, string[]> = {};
+      keys.forEach(key => {
+        const [columnId, id] = key.split("=");
+        getOrSet(columns, columnId, []).push(id);
+      });
+
+      // Or together `where tag_id in (...)` and `book_id in (...)`
+      let query = knex.select("*").from(joinTableName);
+      Object.entries(columns).forEach(([columnId, values]) => {
+        query = query.orWhereIn(
+          columnId,
+          values.map(id => keyToNumber(id)!),
+        );
+      });
+
+      const rows = (await query.orderBy("id")) as JoinRow[];
+
+      // Make a map that will be both `tag_id=2 -> [...]` and `book_id=3 -> [...]`
+      const rowsByKey: Record<string, JoinRow[]> = {};
+
+      // Each join table row might have some promises to load the other entity. Eventually
+      // We should just pull the other side in via the ^ query.
+      const p = rows.map(async row => {
+        // For this join table row, resolve the entities both of foreign keys. For now
+        // we're using the EntityManager.load to do this in batch for this, but eventually
+        // we should pull those into the row itself.
+        const p = Object.entries(row).map(async entry => {
+          const [column, value] = entry;
+          if (column === "id" || column === "created_at") {
+            return;
+          }
+
+          // We'll do this twice per join table row, but in batch.
+          const cstr = (column === collection.columnName
+            ? collection.entity.__orm.metadata.cstr
+            : collection.otherType) as EntityConstructor<any>;
+
+          row[column] = await em.load(cstr, keyToString(value)!);
+
+          // Put this row into the map for both join table columns, i.e. `book_id=2` and `tag_id=3`
+          const key = `${column}=${value}`;
+          getOrSet(rowsByKey, key, []).push(row);
+        });
+        await Promise.all(p);
+      });
+      await Promise.all(p);
+
+      return keys.map(key => {
+        const [column] = key.split("=");
+        const joinRows = rowsByKey[key] || [];
+        const otherColumn = column === collection.columnName ? collection.otherColumnName : collection.columnName;
+        return joinRows.map(joinRow => joinRow[otherColumn] as Entity);
+      });
+    });
+  });
+}
+
+type JoinRow = { id: number; created_at: Date } & { [column: string]: number | Entity };

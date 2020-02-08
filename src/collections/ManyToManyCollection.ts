8e874884333df77a6dacb1e7efdf659d77072f36
Stephen Haberman <stephen.haberman@gmail.com>
2020-02-08 15:49:38 -0600

    Extract method.

--- src/collections/ManyToManyCollection.ts
+++ src/collections/ManyToManyCollection.ts
@@ -36,61 +36,70 @@ function loaderForJoinTable<T extends Entity, U extends Entity>(collection: Many
   const { joinTableName } = collection;
   const { em } = collection.entity.__orm;
   return getOrSet(em.loaders, joinTableName, () => {
-    return new DataLoader<string, Entity[]>(async keys => {
-      // Break out `column_id=string` keys out
-      const columns: Record<string, string[]> = {};
-      keys.forEach(key => {
-        const [columnId, id] = key.split("=");
-        getOrSet(columns, columnId, []).push(id);
-      });
+    return new DataLoader<string, Entity[]>(async keys => loadFromJoinTable(collection, keys));
+  });
+}
 
-      // Or together `where tag_id in (...)` and `book_id in (...)`
-      let query = em.knex.select("*").from(joinTableName);
-      Object.entries(columns).forEach(([columnId, values]) => {
-        query = query.orWhereIn(
-          columnId,
-          values.map(id => keyToNumber(id)!),
-        );
-      });
+/** Loads join rows. */
+async function loadFromJoinTable<T extends Entity, U extends Entity>(
+  collection: ManyToManyCollection<T, U>,
+  keys: ReadonlyArray<string>,
+): Promise<Entity[][]> {
+  const { joinTableName } = collection;
+  const { em } = collection.entity.__orm;
 
-      const rows = (await query.orderBy("id")) as JoinRow[];
+  // Break out `column_id=string` keys out
+  const columns: Record<string, string[]> = {};
+  keys.forEach(key => {
+    const [columnId, id] = key.split("=");
+    getOrSet(columns, columnId, []).push(id);
+  });
 
-      // Make a map that will be both `tag_id=2 -> [...]` and `book_id=3 -> [...]`
-      const rowsByKey: Record<string, JoinRow[]> = {};
+  // Or together `where tag_id in (...)` and `book_id in (...)`
+  let query = em.knex.select("*").from(joinTableName);
+  Object.entries(columns).forEach(([columnId, values]) => {
+    query = query.orWhereIn(
+      columnId,
+      values.map(id => keyToNumber(id)!),
+    );
+  });
 
-      // Each join table row might have some promises to load the other entity. Eventually
-      // we should just pull the other side in via the ^ query (even if some of the rows
-      // might already be in our UoW).
-      const p = rows.map(async row => {
-        // For this join table row, resolve the entities both of foreign keys. For now
-        // we're using the EntityManager.load to do this for this (in batch so it is N+1 safe),
-        // but eventually we should pull those into the row itself.
-        const p = Object.entries(row).map(async entry => {
-          const [column, value] = entry;
-          if (column === "id" || column === "created_at") {
-            return;
-          }
+  const rows = (await query.orderBy("id")) as JoinRow[];
 
-          const cstr = (column === collection.columnName
-            ? collection.entity.__orm.metadata.cstr
-            : collection.otherType) as EntityConstructor<any>;
-          row[column] = await em.load(cstr, keyToString(value)!);
+  // Make a map that will be both `tag_id=2 -> [...]` and `book_id=3 -> [...]`
+  const rowsByKey: Record<string, JoinRow[]> = {};
 
-          // Put this row into the map for both join table columns, i.e. `book_id=2` and `tag_id=3`
-          const key = `${column}=${value}`;
-          getOrSet(rowsByKey, key, []).push(row);
-        });
-        await Promise.all(p);
-      });
-      await Promise.all(p);
+  // Each join table row might have some promises to load the other entity. Eventually
+  // we should just pull the other side in via the ^ query (even if some of the rows
+  // might already be in our UoW).
+  const p = rows.map(async row => {
+    // For this join table row, resolve the entities both of foreign keys. For now
+    // we're using the EntityManager.load to do this for this (in batch so it is N+1 safe),
+    // but eventually we should pull those into the row itself.
+    const p = Object.entries(row).map(async entry => {
+      const [column, value] = entry;
+      if (column === "id" || column === "created_at") {
+        return;
+      }
 
-      // Map the requested keys, i.e. book_id=2 back to "the tags for book 2".
-      return keys.map(key => {
-        const [column] = key.split("=");
-        const joinRows = rowsByKey[key] || [];
-        const otherColumn = column === collection.columnName ? collection.otherColumnName : collection.columnName;
-        return joinRows.map(joinRow => joinRow[otherColumn] as Entity);
-      });
+      const cstr = (column === collection.columnName
+        ? collection.entity.__orm.metadata.cstr
+        : collection.otherType) as EntityConstructor<any>;
+      row[column] = await em.load(cstr, keyToString(value)!);
+
+      // Put this row into the map for both join table columns, i.e. `book_id=2` and `tag_id=3`
+      const key = `${column}=${value}`;
+      getOrSet(rowsByKey, key, []).push(row);
     });
+    await Promise.all(p);
+  });
+  await Promise.all(p);
+
+  // Map the requested keys, i.e. book_id=2 back to "the tags for book 2".
+  return keys.map(key => {
+    const [column] = key.split("=");
+    const joinRows = rowsByKey[key] || [];
+    const otherColumn = column === collection.columnName ? collection.otherColumnName : collection.columnName;
+    return joinRows.map(joinRow => joinRow[otherColumn] as Entity);
   });
 }

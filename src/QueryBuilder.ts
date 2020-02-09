e0d1c9db62c6e0b0d942ac7ad9743e149202d83b
Stephen Haberman <stephen.haberman@gmail.com>
2020-02-09 09:03:52 -0600

    Add comment.

--- src/QueryBuilder.ts
+++ src/QueryBuilder.ts
@@ -2,6 +2,13 @@ import Knex, { QueryBuilder } from "knex";
 import { fail } from "./utils";
 import { Entity, EntityConstructor, FilterQuery, getMetadata } from "./EntityManager";
 
+/**
+ * Builds the SQL/knex queries for `EntityManager.find` calls.
+ *
+ * Note this is generally for our own internal implementation details and not meant to
+ * be a user-facing QueryBuilder, i.e. users should use Knex for that and just use SQL
+ * directly (for any non-trivial queries that `EntityManager.find` does not support).
+ */
 export function buildQuery<T extends Entity>(
   knex: Knex,
   type: EntityConstructor<T>,

dc55c28dd81aa026ddd6d2722b2a2763d3dfa73b
Stephen Haberman <stephen.haberman@gmail.com>
2020-02-18 00:05:03 -0600

    Put back commented out code.

--- release.sh
+++ release.sh
@@ -2,16 +2,17 @@
 
 set -e
 
-#for p in codegen migration-utils orm ; do
-#  ./bump.sh ./packages/$p/package.json
-#done
+for p in codegen migration-utils orm ; do
+  ./bump.sh ./packages/$p/package.json
+done
 
-#for p in codegen migration-utils orm ; do
-#  cd ./packages/$p
-#  yarn publish --non-interactive --otp $1
-#  cd ../..
-#done
+for p in codegen migration-utils orm ; do
+  cd ./packages/$p
+  yarn publish --non-interactive --otp $1
+  cd ../..
+done
 
 for p in codegen migration-utils orm ; do
   rm ./packages/$p/package.json.bak
+  git checkout ./packages/$p/package.json
 done

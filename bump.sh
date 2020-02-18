266618a3890b0ebd8d45114ba5d851b70ca40ebd
Stephen Haberman <stephen.haberman@gmail.com>
2020-02-17 22:38:44 -0600

    Add bump.

---
+++ bump.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Generates a version number based on the current pattern + number of git commits.
+#
+# Currently hard-coded to make 0.N.X releases where N == the existing minor digit
+# in the file and X == the number of git commits.
+#
+# Usage:
+#   ./bump.sh package.json
+#
+
+file=$1
+pattern=$(grep -o "0\.[[:digit:]]\+\.0-bump" < "${file}")
+minor=$(echo "${pattern}"| grep -o "^0\.[[:digit:]]\+")
+patch=$(git log --first-parent --format=%H | wc -l)
+version="${minor}.${patch}"
+sed -i.bak "s/${pattern}/${version}/" "${file}"

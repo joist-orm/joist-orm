7556647134a1bbc4b89a3ea87566070148ffcc85
Stephen Haberman <stephen.haberman@gmail.com>
2020-02-29 15:10:22 -0600

    Comment, extract methods.

--- packages/codegen/src/EntityDbMetadata.ts
+++ packages/codegen/src/EntityDbMetadata.ts
@@ -1,23 +1,28 @@
-import { SymbolSpec } from "ts-poet/build/SymbolSpecs";
-import { Table } from "pg-structure";
 import { camelCase } from "change-case";
-import { imp } from "ts-poet";
+import { Table, M2ORelation, M2MRelation, O2MRelation, Column } from "pg-structure";
 import pluralize from "pluralize";
+import { imp } from "ts-poet";
+import { SymbolSpec } from "ts-poet/build/SymbolSpecs";
 import { ColumnMetaData } from "./generateEntityCodegenFile";
 import { isEnumTable, isJoinTable, mapSimpleDbType, tableToEntityName } from "./utils";
 
 // TODO Populate from config
 const columnCustomizations: Record<string, ColumnMetaData> = {};
 
+/** Codegen-time metadata about a given domain entity. */
 type Entity = {
   name: string;
+  /** The symbol pointing to the entity itself. */
   type: SymbolSpec;
+  /** The name of the entity's runtime metadata const. */
   metaName: string;
+  /** The symbol pointing to the entity's runtime metadata const. */
   metaType: SymbolSpec;
+  /** The symbol pointing to the entity's EntityId type. */
   idType: SymbolSpec;
 };
 
-type PrimitiveColumn = {
+type PrimitiveField = {
   fieldName: string;
   columnName: string;
   columnType: string;
@@ -25,7 +30,7 @@ type PrimitiveColumn = {
   notNull: boolean;
 };
 
-type EnumColumn = {
+type EnumField = {
   fieldName: string;
   columnName: string;
   enumName: string;
@@ -34,7 +39,7 @@ type EnumColumn = {
   notNull: boolean;
 };
 
-type ManyToOneColumn = {
+type ManyToOneField = {
   fieldName: string;
   columnName: string;
   otherFieldName: string;
@@ -42,14 +47,14 @@ type ManyToOneColumn = {
   notNull: boolean;
 };
 
-type OneToManyColumn = {
+type OneToManyField = {
   fieldName: string;
   otherEntity: Entity;
   otherFieldName: string;
   otherColumnName: string;
 };
 
-type ManyToManyColumn = {
+type ManyToManyField = {
   joinTableName: string;
   fieldName: string;
   columnName: string;
@@ -61,81 +66,87 @@ type ManyToManyColumn = {
 /** Adapts the generally-great pg-structure metadata into our specific ORM types. */
 export class EntityDbMetadata {
   entity: Entity;
-  primitives: PrimitiveColumn[];
-  enums: EnumColumn[];
-  manyToOnes: ManyToOneColumn[];
-  oneToManys: OneToManyColumn[];
-  manyToManys: ManyToManyColumn[];
+  primitives: PrimitiveField[];
+  enums: EnumField[];
+  manyToOnes: ManyToOneField[];
+  oneToManys: OneToManyField[];
+  manyToManys: ManyToManyField[];
 
   constructor(table: Table) {
     this.entity = makeEntity(tableToEntityName(table));
-
     this.primitives = table.columns
       .filter(c => !c.isPrimaryKey && !c.isForeignKey)
-      .map(column => {
-        const fieldName = camelCase(column.name);
-        const columnName = column.name;
-        const columnType = column.type.shortName || column.type.name;
-        const maybeCustomType = mapType(table.name, columnName, columnType);
-        const fieldType = maybeCustomType.fieldType;
-        const notNull = column.notNull;
-        return { fieldName, columnName, columnType, fieldType, notNull };
-      });
+      .map(column => newPrimitive(column, table));
+    this.enums = table.m2oRelations.filter(r => isEnumTable(r.targetTable)).map(r => newEnumField(r));
     this.manyToOnes = table.m2oRelations
       .filter(r => !isEnumTable(r.targetTable))
-      .map(r => {
-        const column = r.foreignKey.columns[0];
-        const columnName = column.name;
-        const fieldName = camelCase(column.name.replace("_id", ""));
-        const otherEntity = makeEntity(tableToEntityName(r.targetTable));
-        const otherFieldName = camelCase(pluralize(this.entity.name));
-        const notNull = column.notNull;
-        return { fieldName, columnName, otherEntity, otherFieldName, notNull };
-      });
-    this.enums = table.m2oRelations
-      .filter(r => isEnumTable(r.targetTable))
-      .map(r => {
-        const column = r.foreignKey.columns[0];
-        const columnName = column.name;
-        const fieldName = camelCase(column.name.replace("_id", ""));
-        const enumName = tableToEntityName(r.targetTable);
-        const enumType = imp(`${enumName}@./entities`);
-        const enumDetailType = imp(`${pluralize(enumName)}@./entities`);
-        const notNull = column.notNull;
-        return { fieldName, columnName, enumName, enumType, enumDetailType, notNull };
-      });
-    // Add OneToMany
+      .map(r => newManyToOneField(this.entity, r));
     this.oneToManys = table.o2mRelations
       // ManyToMany join tables also show up as OneToMany tables in pg-structure
       .filter(r => !isJoinTable(r.targetTable))
-      .map(r => {
-        const column = r.foreignKey.columns[0];
-        // source == parent i.e. the reference of the foreign key column
-        // target == child i.e. the table with the foreign key column in it
-        const otherEntity = makeEntity(tableToEntityName(r.targetTable));
-        // I.e. if the other side is `child.project_id`, use children
-        const fieldName = camelCase(pluralize(otherEntity.name));
-        const otherFieldName = camelCase(column.name.replace("_id", ""));
-        const otherColumnName = column.name;
-        return { fieldName, otherEntity, otherFieldName, otherColumnName };
-      });
+      .map(r => newOneToMany(r));
     this.manyToManys = table.m2mRelations
       // pg-structure is really loose on what it considers a m2m relationship, i.e. any entity
       // that has a foreign key to us, and a foreign key to something else, is automatically
       // considered as a join table/m2m between "us" and "something else". Filter these out
       // by looking for only true join tables, i.e. tables with only id, fk1, and fk2.
       .filter(r => isJoinTable(r.joinTable))
-      .map(r => {
-        const { foreignKey, targetForeignKey, targetTable } = r;
-        const joinTableName = r.joinTable.name;
-        const otherEntity = makeEntity(tableToEntityName(targetTable));
-        const fieldName = camelCase(pluralize(targetForeignKey.columns[0].name.replace("_id", "")));
-        const otherFieldName = camelCase(pluralize(foreignKey.columns[0].name.replace("_id", "")));
-        const columnName = foreignKey.columns[0].name;
-        const otherColumnName = targetForeignKey.columns[0].name;
-        return { joinTableName, fieldName, columnName, otherEntity, otherFieldName, otherColumnName };
-      });
+      .map(r => newManyToManyField(r));
   }
+}
+
+function newPrimitive(column: Column, table: Table): PrimitiveField {
+  const fieldName = camelCase(column.name);
+  const columnName = column.name;
+  const columnType = column.type.shortName || column.type.name;
+  const maybeCustomType = mapType(table.name, columnName, columnType);
+  const fieldType = maybeCustomType.fieldType;
+  const notNull = column.notNull;
+  return { fieldName, columnName, columnType, fieldType, notNull };
+}
+
+function newEnumField(r: M2ORelation): EnumField {
+  const column = r.foreignKey.columns[0];
+  const columnName = column.name;
+  const fieldName = camelCase(column.name.replace("_id", ""));
+  const enumName = tableToEntityName(r.targetTable);
+  const enumType = imp(`${enumName}@./entities`);
+  const enumDetailType = imp(`${pluralize(enumName)}@./entities`);
+  const notNull = column.notNull;
+  return { fieldName, columnName, enumName, enumType, enumDetailType, notNull };
+}
+
+function newManyToOneField(entity: Entity, r: M2ORelation): ManyToOneField {
+  const column = r.foreignKey.columns[0];
+  const columnName = column.name;
+  const fieldName = camelCase(column.name.replace("_id", ""));
+  const otherEntity = makeEntity(tableToEntityName(r.targetTable));
+  const otherFieldName = camelCase(pluralize(entity.name));
+  const notNull = column.notNull;
+  return { fieldName, columnName, otherEntity, otherFieldName, notNull };
+}
+
+function newOneToMany(r: O2MRelation): OneToManyField {
+  const column = r.foreignKey.columns[0];
+  // source == parent i.e. the reference of the foreign key column
+  // target == child i.e. the table with the foreign key column in it
+  const otherEntity = makeEntity(tableToEntityName(r.targetTable));
+  // I.e. if the other side is `child.project_id`, use children
+  const fieldName = camelCase(pluralize(otherEntity.name));
+  const otherFieldName = camelCase(column.name.replace("_id", ""));
+  const otherColumnName = column.name;
+  return { fieldName, otherEntity, otherFieldName, otherColumnName };
+}
+
+function newManyToManyField(r: M2MRelation): ManyToManyField {
+  const { foreignKey, targetForeignKey, targetTable } = r;
+  const joinTableName = r.joinTable.name;
+  const otherEntity = makeEntity(tableToEntityName(targetTable));
+  const fieldName = camelCase(pluralize(targetForeignKey.columns[0].name.replace("_id", "")));
+  const otherFieldName = camelCase(pluralize(foreignKey.columns[0].name.replace("_id", "")));
+  const columnName = foreignKey.columns[0].name;
+  const otherColumnName = targetForeignKey.columns[0].name;
+  return { joinTableName, fieldName, columnName, otherEntity, otherFieldName, otherColumnName };
 }
 
 function makeEntity(entityName: string): Entity {

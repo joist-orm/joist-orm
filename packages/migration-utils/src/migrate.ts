26759df3b0d0b58ea22cacbee9dc116dfc93b3f5
Stephen Haberman <stephen.haberman@gmail.com>
2020-04-09 08:58:48 -0500

    Use DELETEs + ALTER SEQUENCEs instead of TRUNCATEs for flush_database.

--- packages/migration-utils/src/migrate.ts
+++ packages/migration-utils/src/migrate.ts
@@ -41,14 +41,39 @@ async function createFlushDbFunction(db: Db, client: Client): Promise<void> {
 }
 
 function generateFlushFunction(db: Db): string {
-  const tables = db.tables.filter(isEntityTable).map((t) => t.name);
-  const truncate = `TRUNCATE ${tables.join(", ")} RESTART IDENTITY CASCADE`;
-  return `CREATE OR REPLACE FUNCTION flush_database() RETURNS void AS $$ ${truncate} $$ LANGUAGE SQL`;
+  const tables = db.tables.filter((t) => isEntityTable(t) || isJoinTable(t)).map((t) => t.name);
+  // Note that, for whatever reason, doing DELETEs + ALTER SEQUENCEs is dramatically faster than TRUNCATEs.
+  // On even a 40-table schema, TRUNCATEs (either 1 per table or even a single TRUNCATE with all tables) takes
+  // 100s of milliseconds, where as DELETEs takes single-digit milliseconds and DELETEs + ALTER SEQUENCEs is
+  // 10s of milliseconds.
+  //
+  // One cute idea would be to use a single sequence for all tables when running locally. That would
+  // mean our flush_database function could reset a single sequence. Plus it would reduce bugs where
+  // something "works" but only b/c in the test database, all entities have id = 1.
+  const statements = tables
+    .map((t) => `DELETE FROM "${t}"; ALTER SEQUENCE "${t}_id_seq" RESTART WITH 1 INCREMENT BY 1;`)
+    .join("\n");
+  return `CREATE OR REPLACE FUNCTION flush_database() RETURNS void AS $$
+    BEGIN
+    ${statements}
+    END;
+   $$ LANGUAGE
+    'plpgsql'`;
 }
 
 export function isEntityTable(t: Table): boolean {
   const columnNames = t.columns.map((c) => c.name);
   return includesAllOf(columnNames, ["id", "created_at", "updated_at"]);
+}
+
+export function isJoinTable(t: Table): boolean {
+  const { columns } = t;
+  const hasOnePk = columns.filter((c) => c.isPrimaryKey).length === 1;
+  const hasTwoFks = columns.filter((c) => c.isForeignKey).length === 2;
+  const hasThreeColumns = columns.length === 3;
+  const hasFourColumnsOneIsCreatedAt =
+    columns.length === 4 && columns.filter((c) => c.name === "created_at").length === 1;
+  return hasOnePk && hasTwoFks && (hasThreeColumns || hasFourColumnsOneIsCreatedAt);
 }
 
 function includesAllOf(set: string[], subset: string[]): boolean {

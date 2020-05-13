8993196977ecf5e7c494ff68bb90f8915666b60c
Stephen Haberman <stephen.haberman@gmail.com>
2020-05-12 23:51:42 -0500

    Make createOrUpdateUnset recursive, i.e. upsertGraph.

--- packages/integration-tests/src/EntityManager.unsafe.test.ts
+++ packages/integration-tests/src/EntityManager.unsafe.test.ts
@@ -6,25 +6,54 @@ import { insertAuthor } from "./entities/factories";
 describe("EntityManager", () => {
   it("can create new entity with valid data", async () => {
     const em = new EntityManager(knex);
-    const a1 = await createOrUpdateUnsafe(em, Author, null, { firstName: "a1" });
+    const a1 = await createOrUpdateUnsafe(em, Author, { firstName: "a1" });
     expect(a1.firstName).toEqual("a1");
   });
 
   it("fails to create new entity with invalid data", async () => {
     const em = new EntityManager(knex);
-    await expect(createOrUpdateUnsafe(em, Author, null, { firstName: null })).rejects.toThrow("firstName is required");
+    await expect(createOrUpdateUnsafe(em, Author, { id: null, firstName: null })).rejects.toThrow(
+      "firstName is required",
+    );
   });
 
   it("can update an entity with valid data", async () => {
     await insertAuthor({ first_name: "a1" });
     const em = new EntityManager(knex);
-    const a1 = await createOrUpdateUnsafe(em, Author, "1", { firstName: "a2" });
+    const a1 = await createOrUpdateUnsafe(em, Author, { id: "1", firstName: "a2" });
     expect(a1.firstName).toEqual("a2");
   });
 
   it("fails to update an entity with valid data", async () => {
     await insertAuthor({ first_name: "a1" });
     const em = new EntityManager(knex);
-    await expect(createOrUpdateUnsafe(em, Author, "1", { firstName: null })).rejects.toThrow("firstName is required");
+    await expect(createOrUpdateUnsafe(em, Author, { id: "1", firstName: null })).rejects.toThrow(
+      "firstName is required",
+    );
+  });
+
+  it("can create new children with valid data", async () => {
+    const em = new EntityManager(knex);
+    const a1 = await createOrUpdateUnsafe(em, Author, {
+      firstName: "a1",
+      mentor: { firstName: "m1" },
+      books: [{ title: "b1" }],
+    });
+    expect(a1.firstName).toEqual("a1");
+    expect((await a1.mentor.load())!.firstName).toEqual("m1");
+    expect((await a1.books.load())![0].title).toEqual("b1");
+  });
+
+  it("can update existing references with valid data", async () => {
+    await insertAuthor({ first_name: "m1" });
+    const em = new EntityManager(knex);
+    const a1 = await createOrUpdateUnsafe(em, Author, {
+      firstName: "a1",
+      mentor: { id: "1", firstName: "m2" },
+    });
+    expect(a1.firstName).toEqual("a1");
+    expect((await a1.mentor.load())!.firstName).toEqual("m2");
+    await em.flush();
+    expect((await knex.count().from("authors"))[0]).toEqual({ count: "2" });
   });
 });
--- packages/orm/src/index.ts
+++ packages/orm/src/index.ts
@@ -5,6 +5,7 @@ import {
   EntityMetadata,
   getMetadata,
   IdOf,
+  isEntity,
   Loaded,
   LoadHint,
   OptsOf,
@@ -341,20 +342,58 @@ export function newChangesProxy(entity: Entity): any {
   });
 }
 
+// Utility type to strip off null and defined and infer only T.
+type NullOrDefinedOr<T> = T | null | undefined;
+
+type DeepPartialOrNull<T extends Entity> = { id?: IdOf<T> | null } & Foo<PartialOrNull<OptsOf<T>>>;
+
+type Foo<T> = {
+  [P in keyof T]: T[P] extends NullOrDefinedOr<infer U>
+    ? U extends Array<infer V>
+      ? V extends Entity
+        ? Array<DeepPartialOrNull<V>>
+        : T[P]
+      : U extends Entity
+      ? DeepPartialOrNull<U>
+      : T[P]
+    : T[P];
+};
+
 /**
  * A utility function to create-or-update entities coming from a partial-update style API.
  */
 export async function createOrUpdateUnsafe<T extends Entity>(
   em: EntityManager,
   constructor: EntityConstructor<T>,
-  id: IdOf<T> | undefined | null,
-  opts: PartialOrNull<OptsOf<T>>,
+  opts: DeepPartialOrNull<T>,
 ): Promise<T> {
+  const { id, ...others } = opts;
+  const meta = getMetadata(constructor);
+
+  // The values in others might be themselves partials, so walk through and resolve them to entities.
+  const p = Object.entries(others).map(async ([key, value]) => {
+    const field = meta.fields.find((f) => f.fieldName === key)!;
+    if (field.kind === "m2o" && !isEntity(value)) {
+      // This is a many-to-one partial
+      const entity = await createOrUpdateUnsafe(em, field.otherMetadata().cstr, value as any);
+      return [key, entity];
+    } else if (field.kind === "o2m" || field.kind === "m2m") {
+      // Look for one-to-many/many-to-many partials
+      const entities = (value as Array<any>).map(async (value) => {
+        return isEntity(value) ? value : await createOrUpdateUnsafe(em, field.otherMetadata().cstr, value as any);
+      });
+      return [key, await Promise.all(entities)];
+    } else {
+      return [key, value];
+    }
+  });
+  const _opts = Object.fromEntries(await Promise.all(p)) as OptsOf<T>;
+
   if (id === null || id === undefined) {
-    return em.createUnsafe(constructor, opts);
+    return em.createUnsafe(constructor, _opts);
   } else {
     const entity = await em.load(constructor, id);
-    entity.setUnsafe(opts);
+    entity.setUnsafe(_opts);
     return entity;
   }
 }

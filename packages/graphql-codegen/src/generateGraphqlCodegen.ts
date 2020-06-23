0380e08196f320ed13209574f9127fda979f99a0
Stephen Haberman <stephen.haberman@gmail.com>
2020-06-22 21:05:03 -0500

    Add missing comma.

--- packages/graphql-codegen/src/generateGraphqlCodegen.ts
+++ packages/graphql-codegen/src/generateGraphqlCodegen.ts
@@ -7,22 +7,22 @@ export function generateGraphqlCodegen(entities: EntityDbMetadata[], enums: Enum
   const contents = code`
     const mappers = {
       ${entities.map((m) => {
-    return `${m.entity.name}: "@src/entities#${m.entity.idType.value}",`;
-  })}
+        return `${m.entity.name}: "@src/entities#${m.entity.idType.value}",`;
+      })}
       ${Object.entries(enums).map(([_name, rows]) => {
-    const name = pascalCase(_name);
-    return `${name}Detail: "@src/entities#${name}"`;
-  })}
+        const name = pascalCase(_name);
+        return `${name}Detail: "@src/entities#${name},"`;
+      })}
     };
 
     const enumValues = {
       ${Object.keys(enums).map((_name) => {
-    const name = pascalCase(_name);
-    return `${name}: "@src/entities#${name}",`
-  })}
+        const name = pascalCase(_name);
+        return `${name}: "@src/entities#${name}",`;
+      })}
     };
 
     module.exports = { mappers, enumValues };
   `;
-  return {name: "../../graphql-codegen-joist.js", overwrite: true, contents};
+  return { name: "../../graphql-codegen-joist.js", overwrite: true, contents };
 }

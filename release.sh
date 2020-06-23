858dc9b72d42f7d946db78e6647f6abc70b443f2
Stephen Haberman <stephen.haberman@gmail.com>
2020-06-22 20:56:34 -0500

    Add graphql-codegen to publish.

--- release.sh
+++ release.sh
@@ -4,17 +4,17 @@ set -e
 
 echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" > .npmrc
 
-for p in utils codegen migration-utils orm ; do
+for p in utils codegen graphql-codegen migration-utils orm ; do
   ./bump.sh ./packages/$p/package.json
 done
 
-for p in utils codegen migration-utils orm ; do
+for p in utils codegen graphql-codegen migration-utils orm ; do
   cd ./packages/$p
   yarn publish --non-interactive
   cd ../..
 done
 
-for p in utils codegen migration-utils orm ; do
+for p in utils codegen graphql-codegen migration-utils orm ; do
   rm ./packages/$p/package.json.bak
   git checkout ./packages/$p/package.json
 done

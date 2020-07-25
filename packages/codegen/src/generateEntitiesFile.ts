bd1902dd644ea33d2bf849bf1d5bb822a600abb6
Stephen Haberman <stephen.haberman@gmail.com>
2020-07-24 20:51:01 -0500

    Add flag to disable prettier-organize-imports.

--- packages/codegen/src/generateEntitiesFile.ts
+++ packages/codegen/src/generateEntitiesFile.ts
@@ -6,6 +6,8 @@ import { tableToEntityName } from "./utils";
 
 export function generateEntitiesFile(config: Config, entities: EntityDbMetadata[], enums: Table[]): Code {
   return code`
+    // organize-imports-ignore
+
     // This file drives our import order to avoid undefined errors
     // when the subclasses extend the base classes, see:
     // https://medium.com/visual-development/how-to-fix-nasty-circular-dependency-issues-once-and-for-all-in-javascript-typescript-a04c987cf0de
--- packages/integration-tests/src/entities/entities.ts
+++ packages/integration-tests/src/entities/entities.ts
@@ -1,3 +1,5 @@
+// organize-imports-ignore
+
 // This file drives our import order to avoid undefined errors
 // when the subclasses extend the base classes, see:
 // https://medium.com/visual-development/how-to-fix-nasty-circular-dependency-issues-once-and-for-all-in-javascript-typescript-a04c987cf0de

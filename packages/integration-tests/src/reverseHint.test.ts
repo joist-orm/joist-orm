50e2531328898f11340b4fccd5650f3c4e88ea0c
Stephen Haberman <stephen.haberman@gmail.com>
2021-01-27 14:25:00 -0600

    Rerun rules on changes to entities in the middle of a hint.

--- packages/integration-tests/src/reverseHint.test.ts
+++ packages/integration-tests/src/reverseHint.test.ts
@@ -1,5 +1,5 @@
 import { reverseHint as reverse } from "joist-orm";
-import { Author, Book, BookReview, Image } from "./entities";
+import { Author, Book, BookReview, Image, Publisher } from "./entities";
 
 describe("reverseHint", () => {
   it("can do string hint", () => {
@@ -13,8 +13,20 @@ describe("reverseHint", () => {
     ]);
   });
 
-  it("can do array of string", () => {
-    expect(reverse(Author, { books: "reviews" })).toEqual([[BookReview, ["book", "author"]]]);
+  it("can do hash of hints", () => {
+    expect(reverse(Author, { books: "reviews" })).toEqual([
+      [Book, ["author"]],
+      [BookReview, ["book", "author"]],
+    ]);
+  });
+
+  it("returns hints for entities in the middle", () => {
+    expect(reverse(Publisher, { authors: { books: ["author", "reviews"] } })).toEqual([
+      [Author, ["publisher"]],
+      [Book, ["author", "publisher"]],
+      [Author, ["books", "author", "publisher"]],
+      [BookReview, ["book", "author", "publisher"]],
+    ]);
   });
 
   it("can do a o2o relationship from object", () => {

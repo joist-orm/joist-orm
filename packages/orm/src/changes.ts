77a19e95f06745e0a53965ff4ae15555981450b4
Zachary Gavin <zgavin@homebound.com>
2021-04-21 13:12:24 -0600

    added changes.fields to get an array of all changed fields

--- packages/orm/src/changes.ts
+++ packages/orm/src/changes.ts
@@ -17,7 +17,7 @@ type ExcludeNever<T> = Pick<T, { [P in keyof T]: T[P] extends never ? never : P 
  * - Exclude collections
  * - Convert entity types to id types to match what is stored in originalData
  */
-export type Changes<T extends Entity> = ExcludeNever<
+export type Changes<T extends Entity> = { fields: (keyof OptsOf<T>)[] } & ExcludeNever<
   {
     [P in keyof OptsOf<T>]-?: OptsOf<T>[P] extends NullOrDefinedOr<infer U>
       ? U extends Array<any>
@@ -31,9 +31,13 @@ export type Changes<T extends Entity> = ExcludeNever<
 
 export function newChangesProxy<T extends Entity>(entity: T): Changes<T> {
   return new Proxy(entity, {
-    get(target, p: PropertyKey): FieldStatus<any> {
+    get(target, p: PropertyKey): FieldStatus<any> | (keyof OptsOf<T>)[] {
+      if (p === "fields") {
+        return entity.isNewEntity ? [] : (Object.keys(entity.__orm.originalData) as (keyof OptsOf<T>)[]);
+      }
+
       const originalValue = typeof p === "string" && entity.__orm.originalData[p];
-      const hasChanged = typeof p === "string" && p in entity.__orm.originalData && entity.id !== undefined;
+      const hasChanged = typeof p === "string" && p in entity.__orm.originalData && !entity.isNewEntity;
       return { hasChanged, originalValue };
     },
   }) as any;

fc9db7f1a48f1add8dcd29d59c231cec886fd337
Stephen Haberman <stephen.haberman@gmail.com>
2021-10-03 09:29:25 -0500

    Better handle children creating parents via factories.
    I.e. if a parent factory has:
    
    ```
    export function newParent(...) {
      return newTestInstance(em, {
        // Always have one child
        children: [{}],
        ...opts
      });
    }
    ```
    
    But a test is creating the child "bottom up", i.e. doing:
    
    ```
    newChild(em, { parent: ... });
    ```
    
    Then we know the parent is going to get a list of children already (from
    the one that is currently being created), so we don't ned its `children:
    []` default to apply.
    
    So we send in `opts.children: null`, as if the programmer had written
    that by hand, such that the `...opts` convention of the factories will
    write over the `children: [{}]` default.

--- packages/integration-tests/src/entities/Book.factories.ts
+++ packages/integration-tests/src/entities/Book.factories.ts
@@ -1,6 +1,10 @@
 import { EntityManager, FactoryOpts, New, newTestInstance } from "joist-orm";
 import { Book } from "./entities";
 
+// for testing factories
+export let lastBookFactoryOpts: any = null;
+
 export function newBook(em: EntityManager, opts?: FactoryOpts<Book>): New<Book> {
+  lastBookFactoryOpts = opts;
   return newTestInstance(em, Book, opts);
 }

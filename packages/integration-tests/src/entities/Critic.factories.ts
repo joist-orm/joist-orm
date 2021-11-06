08df0e4fed351ee8218db8ea42b7fae027bf7db5
Stephen Haberman <stephen.haberman@gmail.com>
2021-11-06 12:57:26 -0500

    Generate opts as a default value.

--- packages/integration-tests/src/entities/Critic.factories.ts
+++ packages/integration-tests/src/entities/Critic.factories.ts
@@ -1,6 +1,6 @@
 import { EntityManager, FactoryOpts, New, newTestInstance } from "joist-orm";
 import { Critic } from "./entities";
 
-export function newCritic(em: EntityManager, opts?: FactoryOpts<Critic>): New<Critic> {
+export function newCritic(em: EntityManager, opts: FactoryOpts<Critic> = {}): New<Critic> {
   return newTestInstance(em, Critic, opts);
 }

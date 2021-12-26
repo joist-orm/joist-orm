04a60e8962f253fcc5c1ee2950dbc9077377eb4f
Stephen Haberman <stephen.haberman@gmail.com>
2021-12-25 21:35:38 -0600

    Extract config.ts and rules.ts. (#214)

--- packages/orm/src/index.ts
+++ packages/orm/src/index.ts
@@ -7,15 +7,12 @@ import {
   EntityOrmField,
   Field,
   getMetadata,
-  Loaded,
-  LoadHint,
   ManyToManyField,
   ManyToOneField,
   OneToManyField,
   OneToOneField,
   OptsOf,
   PolymorphicField,
-  RelationsIn,
 } from "./EntityManager";
 import { maybeResolveReferenceToId, tagFromId } from "./keys";
 import { Reference } from "./relations";
@@ -26,6 +23,7 @@ import { fail } from "./utils";
 export { newPgConnectionConfig } from "joist-utils";
 export { BaseEntity } from "./BaseEntity";
 export * from "./changes";
+export { ConfigApi, EntityHook } from "./config";
 export { DeepPartialOrNull } from "./createOrUpdatePartial";
 export * from "./drivers";
 export * from "./EntityManager";
@@ -36,6 +34,14 @@ export * from "./newTestInstance";
 export * from "./QueryBuilder";
 export * from "./relations";
 export * from "./reverseHint";
+export {
+  GenericError,
+  newRequiredRule,
+  ValidationError,
+  ValidationErrors,
+  ValidationRule,
+  ValidationRuleResult,
+} from "./rules";
 export * from "./serde";
 export { fail } from "./utils";
 
@@ -199,161 +205,6 @@ export function getRequiredKeys<T extends Entity>(entityOrType: T | EntityConstr
   return getMetadata(entityOrType as any)
     .fields.filter((f) => f.required)
     .map((f) => f.fieldName);
-}
-
-/**
- * Return type of a `ValidationRule`.
- *
- * Consumers can extend `GenericError` to add fields relevant for their application.
- */
-export type ValidationRuleResult<E extends GenericError> = string | E | E[] | undefined;
-
-/** Entity validation errors; if `entity` is invalid, throw a `ValidationError`. */
-export type ValidationRule<T extends Entity> = (entity: T) => MaybePromise<ValidationRuleResult<any>>;
-
-type MaybePromise<T> = T | PromiseLike<T>;
-
-/** A generic error which contains only a message field */
-export type GenericError = { message: string };
-/** An extension to GenericError which associates the error to a specific entity */
-export type ValidationError = { entity: Entity } & GenericError;
-
-export class ValidationErrors extends Error {
-  constructor(public errors: ValidationError[]) {
-    super(errorMessage(errors));
-  }
-}
-
-export function newRequiredRule<T extends Entity>(key: keyof T): ValidationRule<T> {
-  return (entity) => (entity.__orm.data[key] === undefined ? `${key} is required` : undefined);
-}
-
-function errorMessage(errors: ValidationError[]): string {
-  if (errors.length === 1) {
-    return `Validation error: ${errors[0].message}`;
-  } else {
-    return `Validation errors (${errors.length}): ${errors.map((e) => e.message).join(", ")}`;
-  }
-}
-
-export type EntityHook =
-  | "beforeFlush"
-  | "beforeCreate"
-  | "beforeUpdate"
-  | "beforeDelete"
-  | "afterValidation"
-  | "afterCommit";
-type HookFn<T extends Entity, C> = (entity: T, ctx: C) => MaybePromise<void>;
-
-export class ConfigData<T extends Entity, C> {
-  /** The validation rules for this entity type. */
-  rules: ValidationRule<T>[] = [];
-  /** The async derived fields for this entity type. */
-  asyncDerivedFields: Partial<Record<keyof T, [LoadHint<T>, (entity: T) => any]>> = {};
-  /** The hooks for this instance. */
-  hooks: Record<EntityHook, HookFn<T, C>[]> = {
-    beforeDelete: [],
-    beforeFlush: [],
-    beforeCreate: [],
-    beforeUpdate: [],
-    afterCommit: [],
-    afterValidation: [],
-  };
-  // Load-hint-ish structures that point back to instances that depend on us for validation rules.
-  reactiveRules: string[][] = [];
-  // Load-hint-ish structures that point back to instances that depend on us for derived values.
-  reactiveDerivedValues: string[][] = [];
-  cascadeDeleteFields: Array<keyof RelationsIn<T>> = [];
-}
-
-export class ConfigApi<T extends Entity, C> {
-  __data = new ConfigData<T, C>();
-
-  addRule<H extends LoadHint<T>>(populate: H, rule: ValidationRule<Loaded<T, H>>): void;
-  addRule(rule: ValidationRule<T>): void;
-  addRule(ruleOrHint: ValidationRule<T> | any, maybeRule?: ValidationRule<any>): void {
-    if (typeof ruleOrHint === "function") {
-      this.__data.rules.push(ruleOrHint);
-    } else {
-      const fn = async (entity: T) => {
-        const em = getEm(entity);
-        const loaded = await em.populate(entity, ruleOrHint);
-        return maybeRule!(loaded);
-      };
-      // Squirrel our hint away where configureMetadata can find it
-      (fn as any).hint = ruleOrHint;
-      this.__data.rules.push(fn);
-    }
-  }
-
-  cascadeDelete(relationship: keyof RelationsIn<T> & LoadHint<T>): void {
-    this.__data.cascadeDeleteFields.push(relationship);
-    this.beforeDelete(relationship, (entity) => {
-      const relation = entity[relationship] as any as AbstractRelationImpl<any>;
-      relation.maybeCascadeDelete();
-    });
-  }
-
-  /** Registers `fn` as the lambda to provide the async value for `key`. */
-  setAsyncDerivedField<P extends keyof T, H extends LoadHint<T>>(
-    key: P,
-    populate: H,
-    fn: (entity: Loaded<T, H>) => T[P],
-  ): void {
-    this.__data.asyncDerivedFields[key] = [populate, fn as any];
-  }
-
-  private addHook(hook: EntityHook, ruleOrHint: HookFn<T, C> | any, maybeFn?: HookFn<Loaded<T, any>, C>) {
-    if (typeof ruleOrHint === "function") {
-      this.__data.hooks[hook].push(ruleOrHint);
-    } else {
-      const fn = async (entity: T, ctx: C) => {
-        // TODO Use this for reactive beforeFlush
-        const em = getEm(entity);
-        const loaded = await em.populate(entity, ruleOrHint);
-        return maybeFn!(loaded, ctx);
-      };
-      // Squirrel our hint away where configureMetadata can find it
-      (fn as any).hint = ruleOrHint;
-      this.__data.hooks[hook].push(fn);
-    }
-  }
-
-  beforeDelete<H extends LoadHint<T>>(populate: H, fn: HookFn<Loaded<T, H>, C>): void;
-  beforeDelete(fn: HookFn<T, C>): void;
-  beforeDelete(ruleOrHint: HookFn<T, C> | any, maybeFn?: HookFn<Loaded<T, any>, C>): void {
-    this.addHook("beforeDelete", ruleOrHint, maybeFn);
-  }
-
-  beforeFlush<H extends LoadHint<T>>(populate: H, fn: HookFn<Loaded<T, H>, C>): void;
-  beforeFlush(fn: HookFn<T, C>): void;
-  beforeFlush(ruleOrHint: HookFn<T, C> | any, maybeFn?: HookFn<Loaded<T, any>, C>): void {
-    this.addHook("beforeFlush", ruleOrHint, maybeFn);
-  }
-
-  // beforeCreate still needs to take a hint because even though the entity itself is New<T>, we might want to load
-  // a nested relation that isn't loaded yet
-  beforeCreate<H extends LoadHint<T>>(populate: H, fn: HookFn<Loaded<T, H>, C>): void;
-  beforeCreate(fn: HookFn<T, C>): void;
-  beforeCreate(ruleOrHint: HookFn<T, C> | any, maybeFn?: HookFn<Loaded<T, any>, C>): void {
-    this.addHook("beforeCreate", ruleOrHint, maybeFn);
-  }
-
-  beforeUpdate<H extends LoadHint<T>>(populate: H, fn: HookFn<Loaded<T, H>, C>): void;
-  beforeUpdate(fn: HookFn<T, C>): void;
-  beforeUpdate(ruleOrHint: HookFn<T, C> | any, maybeFn?: HookFn<Loaded<T, any>, C>): void {
-    this.addHook("beforeUpdate", ruleOrHint, maybeFn);
-  }
-
-  afterValidation<H extends LoadHint<T>>(populate: H, fn: HookFn<Loaded<T, H>, C>): void;
-  afterValidation(fn: HookFn<T, C>): void;
-  afterValidation(ruleOrHint: HookFn<T, C> | any, maybeFn?: HookFn<Loaded<T, any>, C>): void {
-    this.addHook("afterValidation", ruleOrHint, maybeFn);
-  }
-
-  afterCommit(fn: HookFn<T, C>): void {
-    this.addHook("afterCommit", fn);
-  }
 }
 
 const tagToConstructorMap = new Map<string, EntityConstructor<any>>();

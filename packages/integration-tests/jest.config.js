b8b41d9bb6acc7d63f166c9cfe576450a1bb089a
Stephen Haberman <stephen.haberman@gmail.com>
2022-01-02 13:03:43 -0600

    Use @swc/jest. (#226)

--- packages/integration-tests/jest.config.js
+++ packages/integration-tests/jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  preset: "ts-jest",
+  transform: { "^.+\\.tsx?$": "@swc/jest" },
   moduleNameMapper: {
     "^@src/(.*)": "<rootDir>/src/$1",
     "^src/(.*)": "<rootDir>/src/$1",

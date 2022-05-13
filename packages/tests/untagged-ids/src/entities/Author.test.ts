cf34abb1f816bc2e48204f28095cdb939e35a8bc
Stephen Haberman <stephen.haberman@gmail.com>
2022-05-13 09:09:41 -0500

    Fix tests.

--- packages/tests/untagged-ids/src/entities/Author.test.ts
+++ packages/tests/untagged-ids/src/entities/Author.test.ts
@@ -8,7 +8,7 @@ describe("Author", () => {
     await insertAuthor({ first_name: "a1" });
     const em = newEntityManager();
     const a1 = await em.find(Author, {});
-    expect(a1[0].id).toEqual("a:20000000-0000-0000-0000-000000000000");
+    expect(a1[0].id).toEqual("20000000-0000-0000-0000-000000000000");
   });
 
   it("can create entities with deterministic uuids", async () => {
@@ -16,14 +16,14 @@ describe("Author", () => {
     const a1 = newAuthor(em);
     const a2 = newAuthor(em);
     await em.flush();
-    expect(a1.id).toEqual("a:10000000-0000-0000-0000-000000000000");
-    expect(a2.id).toEqual("a:10000000-0000-0000-0000-000000000001");
+    expect(a1.id).toEqual("10000000-0000-0000-0000-000000000000");
+    expect(a2.id).toEqual("10000000-0000-0000-0000-000000000001");
   });
 
   it("can generate random uuids", async () => {
     const em = newEntityManager({ idAssigner: new RandomUuidAssigner() });
     const a1 = newAuthor(em);
     await em.flush();
-    expect(a1.idOrFail.startsWith("a:")).toBeTruthy();
+    expect(a1.idOrFail.startsWith("a:")).toBe(false);
   });
 });

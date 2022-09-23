6167bc6da259ade7c6aec114a74ffa648b13041b
Stephen Haberman <stephen.haberman@gmail.com>
2022-09-22 21:06:23 -0500

    Remove debug code.

--- packages/integration-tests/src/resolvers/mutations/book/saveBookResolver.test.ts
+++ packages/integration-tests/src/resolvers/mutations/book/saveBookResolver.test.ts
@@ -10,12 +10,4 @@ describe("saveBook", () => {
   });
 });
 
-type MaybePromise<T> = T | Promise<T>;
-type Resolver<R, A, T> = (root: R, args: A, ctx: any, info: any) => MaybePromise<T>;
-type T = typeof saveBook;
-type T1 = T[keyof T];
-type MutationInput<T> = T[keyof T] extends Resolver<any, { input: infer I }, any> ? I : 2;
-type T2 = MutationInput<typeof saveBook>;
-type T3 = typeof saveBook["saveBook"] extends Resolver<any, infer A, any> ? A : 2;
-
 const runSaveBook = makeRunInputMutation(saveBook);

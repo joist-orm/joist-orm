956c0edbbb9e4e7579c2416f59b7a3968dc144bf
Stephen Haberman <stephen.haberman@gmail.com>
2022-09-23 12:43:59 -0500

    fix: Fix export import.

--- packages/graphql-resolver-utils/src/index.ts
+++ packages/graphql-resolver-utils/src/index.ts
@@ -1,6 +1,2 @@
 export { entityResolver } from "./entityResolver";
-export { makeRunInputMutation } from "./makeRunInputMutation";
-export { makeRunObject } from "./makeRunObject";
-export { makeRunObjectFields } from "./makeRunObjectFields";
-export { makeRunQuery } from "./makeRunQuery";
 export { saveEntities, saveEntity } from "./saveEntity";
--- packages/integration-tests/src/resolvers/testUtils.ts
+++ packages/integration-tests/src/resolvers/testUtils.ts
@@ -1,2 +1,2 @@
-export { makeRunInputMutation, makeRunObject, makeRunObjectFields } from "joist-graphql-resolver-utils";
+export { makeRunInputMutation, makeRunObject, makeRunObjectFields } from "joist-graphql-resolver-utils/tests";
 export { run } from "joist-test-utils";

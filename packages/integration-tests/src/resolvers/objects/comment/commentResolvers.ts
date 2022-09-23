467993cb12b9be26b1fba025a9bffb9dca4125ac
Stephen Haberman <stephen.haberman@gmail.com>
2022-09-22 20:30:13 -0500

    Fix comment.

--- packages/integration-tests/src/resolvers/objects/comment/commentResolvers.ts
+++ packages/integration-tests/src/resolvers/objects/comment/commentResolvers.ts
@@ -4,5 +4,5 @@ import { CommentResolvers } from "src/generated/graphql-types";
 import { entityResolver } from "src/resolvers/utils";
 
 export const commentResolvers: CommentResolvers = {
-  ...entityResolver(getMetadata(Comment)),
+  ...entityResolver(getMetadata(Comment), { comment: "text" }),
 };

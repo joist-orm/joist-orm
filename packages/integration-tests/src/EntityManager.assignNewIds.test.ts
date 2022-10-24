3d1fd96a4b3416d435d48abae29925bf237a8625
Chris Fox <cfox@homebound.com>
2022-10-24 15:10:16 -0400

    fix: loadHints.isNew was checking for idTagged===undefined (#458)

--- packages/integration-tests/src/EntityManager.assignNewIds.test.ts
+++ packages/integration-tests/src/EntityManager.assignNewIds.test.ts
@@ -1,5 +1,6 @@
 import { newPublisher } from "./entities";
 import { newEntityManager } from "./setupDbTests";
+import { isNew } from "joist-orm";
 
 describe("EntityManager.assignNewIds", () => {
   it("can assign entity IDs on request", async () => {
@@ -14,11 +15,13 @@ describe("EntityManager.assignNewIds", () => {
     // Then the ID was set appropriately and the entity is still considered new
     expect(p1.id).toEqual("p:1");
     expect(p1.isNewEntity).toEqual(true);
+    expect(isNew(p1)).toEqual(true);
 
     // And when I flush
     await em.flush();
     // Then the id remains the same and the entity is no longer new
     expect(p1.id).toEqual("p:1");
     expect(p1.isNewEntity).toEqual(false);
+    expect(isNew(p1)).toEqual(false);
   });
 });

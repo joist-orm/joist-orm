e9404ae68783d1247f52768e54d29fad58519442
Stephen Haberman <stephen.haberman@gmail.com>
2023-01-17 13:12:12 -0600

    fix: Tests setting undefined should always win.

--- packages/integration-tests/src/entities/Author.factories.ts
+++ packages/integration-tests/src/entities/Author.factories.ts
@@ -4,14 +4,13 @@ import { Author } from "./entities";
 // for testing factories
 export let lastAuthorFactoryOpts: any = null;
 
-export function newAuthor(em: EntityManager, opts?: FactoryOpts<Author>): DeepNew<Author> {
+export function newAuthor(em: EntityManager, opts: FactoryOpts<Author> = {}): DeepNew<Author> {
   lastAuthorFactoryOpts = opts;
-  return newTestInstance(em, Author, {
+  return newTestInstance(em, Author, opts, {
     firstName: `a${testIndex}`,
     // example of using opts
     age: opts?.isPopular ? 50 : undefined,
     // Adding to ensure newTestInstance doesn't infinitely recurse between m2os & o2os
     image: undefined,
-    ...opts,
   });
 }

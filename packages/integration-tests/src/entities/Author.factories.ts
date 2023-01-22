a61186afe645b90f417ed4dbb1c3e96000c28fec
Stephen Haberman <stephen.haberman@gmail.com>
2023-01-22 13:53:49 -0600

    Remove unnecessary coalesce.

--- packages/integration-tests/src/entities/Author.factories.ts
+++ packages/integration-tests/src/entities/Author.factories.ts
@@ -9,7 +9,7 @@ export function newAuthor(em: EntityManager, opts: FactoryOpts<Author> = {}): De
   return newTestInstance(em, Author, opts, {
     firstName: `a${testIndex}`,
     // example of using opts, and also used as an example of ignoring opts
-    age: opts?.isPopular ? 50 : undefined,
+    age: opts.isPopular ? 50 : undefined,
     // Adding to ensure newTestInstance doesn't infinitely recurse between m2os & o2os
     image: undefined,
   });

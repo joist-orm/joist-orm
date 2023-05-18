328f6667f0cebb6302bb306ecf6d0cfefd8c23e4
Edwar Plata <eplata@homebound.com>
2023-05-18 09:31:32 -0500

    Update EntityManager.reactiveRules.test.tsx

--- packages/integration-tests/src/EntityManager.reactiveRules.test.tsx
+++ packages/integration-tests/src/EntityManager.reactiveRules.test.tsx
@@ -184,24 +184,25 @@ describe("EntityManager.reactiveRules", () => {
     const b = newBook(em, { author: a, reviews: [{ rating: 10 }] });
     await em.flush();
 
-    // For some reason .toMatchEntity(b) fails here
-    // expect(a.favoriteBook.get).toMatchEntity(b);
-    expect(a.favoriteBook.get).toBe(b);
+    // Then the derived field is updated
+    // We will check the database directly to make sure the FK is updated
     expect(await select("authors")).toMatchObject([{ id: 1, favorite_book_id: 1 }]);
+    // And then we will assert using joist's .get (as this might cause the derived field to be recalculated)
+    expect(a.favoriteBook.get).toBe(b);
 
     // If there is a new favorite book
     const b2 = newBook(em, { author: a, reviews: [{ rating: 20 }] });
     await em.flush();
     // Then the derived field is updated
-    expect(a.favoriteBook.get).toBe(b2);
     expect(await select("authors")).toMatchObject([{ id: 1, favorite_book_id: 2 }]);
+    expect(a.favoriteBook.get).toBe(b2);
 
     // If the favorite book is deleted
     em.delete(b2);
     await em.flush();
     // Then the derived field is updated
-    expect(a.favoriteBook.get).toBe(b);
     expect(await select("authors")).toMatchObject([{ id: 1, favorite_book_id: 1 }]);
+    expect(a.favoriteBook.get).toBe(b);
   });
 
   it.withCtx("creates the right reactive derived values", async () => {

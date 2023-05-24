28bd5915a24510df30c63d36655659cc3b69e71a
Stephen Haberman <stephen.haberman@gmail.com>
2023-05-24 08:27:08 -0500

    feat: Teach em.findOrCreate to look for newly-created/updated entities (#661)
    * feat: Teach findOrCreate to look for new entities.
    
    The "WHERE clause" of findOrCreate is simply enough that we can eval
    it in memory against any newly created entities.
    
    * Do the in-memory check first.

--- packages/orm/src/dataloaders/findOrCreateDataLoader.ts
+++ packages/orm/src/dataloaders/findOrCreateDataLoader.ts
@@ -1,7 +1,10 @@
 import DataLoader from "dataloader";
 import { Entity } from "../Entity";
 import { FilterWithAlias } from "../EntityFilter";
-import { EntityConstructor, EntityManager, OptsOf, TooManyError } from "../EntityManager";
+import { EntityConstructor, EntityManager, OptsOf, sameEntity, TooManyError } from "../EntityManager";
+import { getMetadata } from "../EntityMetadata";
+import { isLoadedReference, ManyToOneReference, PolymorphicReference } from "../relations";
+import { fail } from "../utils";
 import { whereFilterHash } from "./findDataLoader";
 
 interface Key<T extends Entity> {
@@ -29,6 +32,21 @@ export function findOrCreateDataLoader<T extends Entity>(
       // asking for the entity to be created/updated slightly differently.
       return Promise.all(
         keys.map(async ({ where, ifNew, upsert }) => {
+          // Before we find/create an entity, see if we have a maybe-new one in the EM already.
+          // This will also use any WIP changes we've made to the found entity, which ideally is
+          // something `em.find` would do as well, but its queries are much more complex..
+          const inMemory = em.entities.filter((e) => e instanceof type && entityMatches(e, where));
+          if (inMemory.length > 1) {
+            throw new TooManyError();
+          } else if (inMemory.length === 1) {
+            const entity = inMemory[0] as T;
+            if (upsert) {
+              entity.set(upsert);
+            }
+            return entity;
+          }
+
+          // If we didn't find it in the EM, do the db query/em.create
           const entities = await em.find(type, where as FilterWithAlias<T>, { softDeletes });
           let entity: T;
           if (entities.length > 1) {
@@ -48,4 +66,31 @@ export function findOrCreateDataLoader<T extends Entity>(
     // Our filter tuple is a complex object, so object-hash it to ensure caching works
     { cacheKeyFn: whereFilterHash },
   );
+}
+
+/** A simple in-memory match of `entity` against a mini/flat "WHERE clause" of `opts`. */
+function entityMatches<T extends Entity>(entity: T, opts: Partial<OptsOf<T>>): boolean {
+  const meta = getMetadata(entity);
+  return Object.entries(opts).every(([fieldName, value]) => {
+    const field = meta.allFields[fieldName] ?? fail(`Invalid field ${fieldName}`);
+    const fn = fieldName as keyof T & keyof OptsOf<T>;
+    switch (field.kind) {
+      case "primaryKey":
+      case "primitive":
+      case "enum":
+        return entity[fn] === opts[fn];
+      case "m2o":
+      case "poly":
+        const relation = entity[fn] as ManyToOneReference<T, any, any> | PolymorphicReference<T, any, any>;
+        if (isLoadedReference(relation)) {
+          // Prefer using `.get` because it will handle new/id-less entities
+          return sameEntity(relation.get, (opts as any)[fn]);
+        } else {
+          // Otherwise use ids
+          return sameEntity(relation.id as any, (opts as any)[fn]);
+        }
+      default:
+        throw new Error(`Unsupported field ${fieldName}`);
+    }
+  });
 }

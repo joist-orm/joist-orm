b3c49bd552538acd63b8e93ea78d26cf4bd5ba8a
James Canning <jamesocanning@gmail.com>
2023-06-16 22:49:34 +0100

    fix: add toJSON to ValidationErrors (#693)

--- packages/orm/src/rules.ts
+++ packages/orm/src/rules.ts
@@ -35,11 +35,15 @@ export type ValidationError = { entity: Entity } & GenericError;
 
 export class ValidationErrors extends Error {
   public errors: ValidationError[];
+  public readonly toJSON: () => string;
   constructor(errors: ValidationError[]);
   constructor(message: string);
   constructor(messageOrErrors: ValidationError[] | string) {
     super(typeof messageOrErrors === "string" ? messageOrErrors : errorMessage(messageOrErrors));
     this.errors = typeof messageOrErrors === "string" ? [] : messageOrErrors;
+    // Jest clones without prototype, so explictly setting this as a property rather than a class method
+    // https://github.com/jestjs/jest/issues/11958
+    this.toJSON = () => `ValidationErrors: ${this.message}`;
   }
 }
 

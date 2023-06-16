4f50e309aa8d49665ff8b62542511e9875a7d067
James Canning <jamesocanning@gmail.com>
2023-06-16 22:39:03 +0100

    Update the test too

--- packages/integration-tests/src/rules.test.ts
+++ packages/integration-tests/src/rules.test.ts
@@ -2,6 +2,6 @@ import { ValidationErrors } from "joist-orm";
 
 describe("ValidationErrors", () => {
   it("serialises even with prototype lost", () => {
-    expect(JSON.stringify({ ...new ValidationErrors("example") })).toEqual(`"<ValidationErrors> example"`);
+    expect(JSON.stringify({ ...new ValidationErrors("example") })).toEqual(`"ValidationErrors: example"`);
   });
 });

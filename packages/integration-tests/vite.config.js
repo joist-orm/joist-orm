9b42efe3510ea61babddb3e23fd72ce992216223
Stephen Haberman <stephen.haberman@gmail.com>
2023-09-01 16:12:51 -0500

    Try just singleThread.

--- packages/integration-tests/vite.config.js
+++ packages/integration-tests/vite.config.js
@@ -4,8 +4,9 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     globals: true,
-    threads: false,
+    // threads: false,
     singleThread: true,
+    // isolate: false,
     globalSetup: "./src/setupTestEnv.ts",
     setupFilesAfterEnv: ["./src/setupIt.ts", "./src/setupDbTests.ts"],
     root: "src/",

1ebbbf1a48f8b833b97219dc4de96e833f6e884d
James Canning <jamesocanning@gmail.com>
2023-10-04 18:23:13 +0100

    fix: Fix double quoting in buildKnexQuery (#817)
    * test: add repro for quoting on join
    
    * Wrap col1/col2 in knex.raw.
    
    * Add some more kq/kqDot/raw.
    
    ---------
    
    Co-authored-by: Stephen Haberman <stephen.haberman@gmail.com>

---
+++ packages/integration-tests/src/QueryParser.quotes.test.ts
@@ -0,0 +1,28 @@
+import { Book } from "@src/entities";
+import { knex } from "@src/setupDbTests";
+import { getMetadata, ParsedFindQuery, parseFindQuery } from "joist-orm";
+import { buildKnexQuery } from "joist-orm/build/src/drivers/buildKnexQuery";
+
+function generateSql(t: ParsedFindQuery) {
+  return buildKnexQuery(knex, t, {}).toSQL().sql;
+}
+
+const bm = getMetadata(Book);
+
+describe("QueryParser", () => {
+  it("quotes with abbreviation", () => {
+    expect(generateSql(parseFindQuery(bm, { author: { firstName: "jeff", schedules: { id: 4 } } }))).toEqual(
+      [
+        'select distinct b.*, "b"."title", "b"."id"',
+        " from books as b",
+        " inner join authors as a on b.author_id = a.id",
+        ' left outer join author_schedules as "as" on a.id = "as".author_id',
+        " where b.deleted_at is null",
+        " and a.deleted_at is null",
+        " and a.first_name = ?",
+        ' and "as".id = ?',
+        " order by b.title ASC, b.id ASC",
+      ].join(""),
+    );
+  });
+});

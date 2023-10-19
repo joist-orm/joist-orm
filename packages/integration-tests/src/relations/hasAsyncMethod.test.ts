c12dda084e22c9e62836283ae66cc69e3e1adc5b
Stephen Haberman <stephen.haberman@gmail.com>
2023-10-19 12:56:08 -0500

    feat: Rename AsyncMethod.get to call. (#830)
    To highlight it can have side effects.

--- packages/integration-tests/src/relations/hasAsyncMethod.test.ts
+++ packages/integration-tests/src/relations/hasAsyncMethod.test.ts
@@ -15,37 +15,37 @@ describe("hasAsyncMethod", () => {
     expect(books).toMatchEntity([{ title: "programming in action" }]);
   });
 
-  it("cannot be accessed via a get when unloaded", async () => {
+  it("cannot be accessed via a call when unloaded", async () => {
     await insertAuthor({ first_name: "a1" });
     await insertBook({ author_id: 1, title: "programming in action" });
     // Given a stock author
     const em = newEntityManager();
     const a1 = await em.load(Author, "a:1");
-    // When we access an async method via get, we get a compile error
+    // When we access an async method via call, we get a compile error
     expect(() => {
       // @ts-expect-error
-      a1.booksWithTitle.get("programming");
-    }).toThrow("hasAsyncMethod.get was called but not loaded");
+      a1.booksWithTitle.call("programming");
+    }).toThrow("hasAsyncMethod.call was called but not loaded");
   });
 
-  it("can be accessed via a get when loaded", async () => {
+  it("can be accessed via call when loaded", async () => {
     await insertAuthor({ first_name: "a1" });
     await insertBook({ author_id: 1, title: "programming in action" });
     // Given an author with `booksWithTitle` loaded
     const em = newEntityManager();
     const a1 = await em.load(Author, "a:1", "booksWithTitle");
-    // When we access an async method via get
-    const books = a1.booksWithTitle.get("programming");
+    // When we access an async method via call
+    const books = a1.booksWithTitle.call("programming");
     // Then we get back the expected value
     expect(books).toMatchEntity([{ title: "programming in action" }]);
   });
 
-  it("can be accessed via a get deep new", async () => {
+  it("can be accessed via a call on deep new entities", async () => {
     const em = newEntityManager();
     // Given a factory-created author
     const a1 = newAuthor(em);
-    // When we access an async method via get
-    const books = a1.booksWithTitle.get("programming");
+    // When we access an async method via call
+    const books = a1.booksWithTitle.call("programming");
     // Then we get back the expected value
     expect(books).toMatchEntity([]);
   });
@@ -60,13 +60,13 @@ describe("hasAsyncMethod", () => {
     expect(await a1.booksTitles.load()).toBe("programming in action");
   });
 
-  it("without params can be accessed via get", async () => {
+  it("without params can be accessed via call", async () => {
     await insertAuthor({ first_name: "a1" });
     await insertBook({ author_id: 1, title: "programming in action" });
     // Given a stock author
     const em = newEntityManager();
     const a1 = await em.load(Author, "a:1", "booksTitles");
     // When we access an async method
-    expect(a1.booksTitles.get()).toBe("programming in action");
+    expect(a1.booksTitles.call()).toBe("programming in action");
   });
 });

8225097be026d701b532382b3ad0426d3818d9cf
Edwar Plata <edwarplata@gmail.com>
2023-11-03 12:57:08 -0500

    Update Entity.test.ts

--- packages/integration-tests/src/Entity.test.ts
+++ packages/integration-tests/src/Entity.test.ts
@@ -275,7 +275,7 @@ describe("Entity", () => {
           "afterCommitRan": false,
           "afterValidationRan": false,
           "ageRuleInvoked": 0,
-          "beforeCreateRan": false,
+          "beforeCommit": false,
           "beforeCreateRan": false,
           "beforeDeleteRan": false,
           "beforeFlushRan": false,

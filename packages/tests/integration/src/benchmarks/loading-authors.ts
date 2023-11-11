0c60e8cb27987e50e2511a3ecdfb4bbe498b51dc
Stephen Haberman <stephen.haberman@gmail.com>
2023-11-11 11:01:50 -0600

    chore: Add postgres.js to benchmark.

--- packages/tests/integration/src/benchmarks/loading-authors.ts
+++ packages/tests/integration/src/benchmarks/loading-authors.ts
@@ -1,9 +1,12 @@
 import { Author } from "@src/entities";
-import { newEntityManager, testDriver } from "@src/testEm";
+import { knex, newEntityManager, testDriver } from "@src/testEm";
+import postgres from "postgres";
 
 async function main() {
   const mitata = await import("mitata");
   const { run, bench, group, baseline } = mitata;
+
+  const sql = postgres("postgres://joist:local@localhost:5435/joist", { max: 4 });
 
   /*
   SELECT flush_database();
@@ -11,23 +14,39 @@ async function main() {
   INSERT INTO books (author_id, title) SELECT i, 'b' || i::text FROM generate_series(1, 50000) AS t(i);
    */
 
-  group("loading one level", () => {
-    bench("authors", async () => {
+  // group("loading knex", () => {
+  //   bench("authors", async () => {
+  //     await knex.select("*").from("authors");
+  //   });
+  // });
+
+  group("loading 50k authors", () => {
+    bench("postgres.js", async () => {
+      await sql`select * from authors`;
+    });
+
+    bench("knex", async () => {
+      await knex.select("*").from("authors");
+    });
+
+    bench("em.find", async () => {
       const em = newEntityManager();
       await em.find(Author, {});
     });
   });
 
-  group("loading two levels", () => {
-    bench("authors", async () => {
-      const em = newEntityManager();
-      await em.find(Author, { id: { lt: 20_000 } } as any, { populate: "books" });
-    });
-  });
+  // group("loading two levels", () => {
+  //   bench("authors", async () => {
+  //     const em = newEntityManager();
+  //     await em.find(Author, { id: { lt: 20_000 } } as any, { populate: "books" });
+  //   });
+  // });
 
   await run({});
   await testDriver.destroy();
+  await sql.end();
 }
 
+// yarn clinic flame -- node --env-file .env --import=tsx ./src/benchmarks/loading-authors.ts
 // yarn env-cmd tsx ./src/benchmarks/loading-authors.ts
 main();

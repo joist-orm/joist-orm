ff6d30894fea99d188c8beb4df786ed1ad5d20df
Stephen Haberman <stephen.haberman@gmail.com>
2023-11-11 12:31:13 -0600

    feat: Add a Zod/json-schema config for joist-config.json file (#864)
    * feat: Use zod for config validation.
    
    * Use strict.
    
    * Remove some defaults, generate the JSON schema.
    
    * Pretty errors.

--- packages/codegen/src/config.ts
+++ packages/codegen/src/config.ts
@@ -3,89 +3,104 @@ import { getBuffer } from "@dprint/json";
 import { DbMetadata, Entity } from "EntityDbMetadata";
 import { promises as fs } from "fs";
 import { groupBy } from "joist-utils";
+import { z } from "zod";
 import { fail, sortKeys, trueIfResolved } from "./utils";
 
 const jsonFormatter = createFromBuffer(getBuffer());
 
-export interface FieldConfig {
-  derived?: "sync" | "async";
-  protected?: boolean;
-  ignore?: true;
-  superstruct?: string;
-  zodSchema?: string;
-  type?: string;
-  serde?: string;
-}
+const fieldConfig = z
+  .object({
+    derived: z.optional(z.union([z.literal("sync"), z.literal("async")])),
+    protected: z.optional(z.boolean()),
+    ignore: z.optional(z.boolean()),
+    superstruct: z.optional(z.string()),
+    zodSchema: z.optional(z.string()),
+    type: z.optional(z.string()),
+    serde: z.optional(z.string()),
+  })
+  .strict();
 
-export interface RelationConfig {
-  polymorphic?: "notNull" | true;
-  large?: true;
-  orderBy?: string;
-}
+export type FieldConfig = z.infer<typeof fieldConfig>;
 
-export interface EntityConfig {
-  tag: string;
-  tableName?: string;
-  fields?: Record<string, FieldConfig>;
-  relations?: Record<string, RelationConfig>;
-  /** Whether this entity should be abstract, e.g. for inheritance a subtype must be instantiated instead of this type. */
-  abstract?: boolean;
-  orderBy?: string;
-}
+const relationConfig = z
+  .object({
+    polymorphic: z.optional(z.union([z.literal("notNull"), z.literal(true)])),
+    large: z.optional(z.boolean()),
+    orderBy: z.optional(z.string()),
+  })
+  .strict();
 
-export interface TimestampConfig {
-  /** The names to check for this timestamp, i.e. `created_at` `created`, etc. */
-  names: string[];
-  /** Whether this timestamp column is required to consider a table an entity, defaults to `false`. */
-  required?: boolean;
-}
+export type RelationConfig = z.infer<typeof relationConfig>;
 
-export interface Config {
-  /** The _build-time_ database URL for reading database metadata. */
-  databaseUrl?: string;
+const entityConfig = z
+  .object({
+    tag: z.string(),
+    tableName: z.optional(z.string()),
+    fields: z.optional(z.record(fieldConfig)),
+    relations: z.optional(z.record(relationConfig)),
+    /** Whether this entity should be abstract, e.g. for inheritance a subtype must be instantiated instead of this type. */
+    abstract: z.optional(z.boolean()),
+    orderBy: z.optional(z.string()),
+  })
+  .strict();
 
-  /** Your application's request-level `Context` type. */
-  contextType?: string;
+export type EntityConfig = z.infer<typeof entityConfig>;
 
-  /**
-   * Allows the user to specify the `updated_at` / `created_at` column names to look up, and if they're optional.
-   *
-   * We default to looking for `updated_at`, `updatedAt`, `created_at`, `createdAt`, and optional to true,
-   * e.g. tables are not required to have both timestamp columns to be considered entities.
-   *
-   * These defaults are the most lenient, to facilitate running Joist against an existing schema and
-   * seeing all of your entities, regardless of your previous conventions.
-   */
-  timestampColumns?: {
-    createdAt?: TimestampConfig;
-    updatedAt?: TimestampConfig;
-    deletedAt?: TimestampConfig;
-  };
-  /**
-   * By default, we create a `flush_database` function for fast testing.
-   *
-   * However, if you don't want to use this, or you have your own bespoke function like we do
-   * that is more application-aware, then you can disable Joist's out-of-the-box one.
-   *
-   * If you have more than one test database, you can set `createFlushFunction` to the array
-   * of test database names, i.e. `mydb_test_1`, `mydb_test_2`, etc.
-   */
-  createFlushFunction?: boolean | string[];
-  entitiesDirectory: string;
-  codegenPlugins: string[];
-  entities: Record<string, EntityConfig>;
-  ignoredTables?: string[];
-  /** The type of entity `id` fields; defaults to `tagged-string`. */
-  idType?: "tagged-string" | "untagged-string" | "number";
+const timestampConfig = z
+  .object({
+    /** The names to check for this timestamp, i.e. `created_at` `created`, etc. */
+    names: z.array(z.string()),
+    /** Whether this timestamp column is required to consider a table an entity, defaults to `false`. */
+    required: z.optional(z.boolean()),
+  })
+  .strict();
+
+export type TimestampConfig = z.infer<typeof timestampConfig>;
+
+export const config = z
+  .object({
+    /** The _build-time_ database URL for reading database metadata. */
+    databaseUrl: z.optional(z.string()),
+    /** Your application's request-level `Context` type. */
+    contextType: z.optional(z.string()),
+    /**
+     * Allows the user to specify the `updated_at` / `created_at` column names to look up, and if they're optional.
+     *
+     * We default to looking for `updated_at`, `updatedAt`, `created_at`, `createdAt`, and optional to true,
+     * e.g. tables are not required to have both timestamp columns to be considered entities.
+     *
+     * These defaults are the most lenient, to facilitate running Joist against an existing schema and
+     * seeing all of your entities, regardless of your previous conventions.
+     */
+    timestampColumns: z.optional(
+      z.object({
+        createdAt: z.optional(timestampConfig),
+        updatedAt: z.optional(timestampConfig),
+        deletedAt: z.optional(timestampConfig),
+      }),
+    ),
+    /**
+     * By default, we create a `flush_database` function for fast testing.
+     *
+     * However, if you don't want to use this, or you have your own bespoke function like we do
+     * that is more application-aware, then you can disable Joist's out-of-the-box one.
+     *
+     * If you have more than one test database, you can set `createFlushFunction` to the array
+     * of test database names, i.e. `mydb_test_1`, `mydb_test_2`, etc.
+     */
+    createFlushFunction: z.optional(z.union([z.boolean(), z.array(z.string())])),
+    entitiesDirectory: z.string().default("./src/entities"),
+    codegenPlugins: z.optional(z.array(z.string())),
+    entities: z.record(entityConfig).default({}),
+    ignoredTables: z.optional(z.array(z.string())),
+    /** The type of entity `id` fields; defaults to `tagged-string`. */
+    idType: z.optional(z.union([z.literal("tagged-string"), z.literal("untagged-string"), z.literal("number")])),
+  })
+  .strict();
+
+export type Config = z.infer<typeof config> & {
   // We don't persist this, and instead just use it as a cache
   __tableToEntityName?: Record<string, string>;
-}
-
-export const defaultConfig: Config = {
-  entitiesDirectory: "./src/entities",
-  codegenPlugins: [],
-  entities: {},
-  __tableToEntityName: {},
 };
 
 export const ormMaintainedFields = ["createdAt", "updatedAt"];
@@ -179,12 +194,17 @@ export async function loadConfig(): Promise<Config> {
   const exists = await trueIfResolved(fs.access(configPath));
   if (exists) {
     const content = await fs.readFile(configPath);
-    return {
-      ...defaultConfig,
-      ...(JSON.parse(content.toString()) as Config),
-    };
+    const result = config.safeParse(JSON.parse(content.toString()));
+    if (!result.success) {
+      throw new Error(
+        `Invalid joist-config.json: ${result.error.errors
+          .map((ze) => `${ze.path.map(String).join("/")} ${ze.message}`)
+          .join("\n")}`,
+      );
+    }
+    return result.data;
   }
-  return defaultConfig;
+  return config.parse({});
 }
 
 /**

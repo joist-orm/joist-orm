065da87468442f7cd3b9ca6b6c364c44202376ff
Stephen Haberman <stephen.haberman@gmail.com>
2023-11-26 15:50:26 -0600

    Comment other methods we'll need.

--- packages/orm/src/plugins/FindPlugin.ts
+++ packages/orm/src/plugins/FindPlugin.ts
@@ -2,5 +2,16 @@ import { EntityMetadata } from "../EntityMetadata";
 import { ParsedFindQuery } from "../QueryParser";
 
 export interface FindPlugin {
+  // Will also need:
+  // beforeDelete
+  // beforeCreate
+  // setField
+  // getField
+  // --> beforeFind might return a callback that says "here are the entities
+  // that you found", so that we could seed our getField/setField caches by
+  // knowing which explicit node they were loaded from. ...if an entity exists
+  // twice in the AuthRule, the callback might need the raw `row` as well, to
+  // integrate which AuthRule the entity came from.
+
   beforeFind(meta: EntityMetadata, query: ParsedFindQuery): void;
 }

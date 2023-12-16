6f4ff6c6070cc9af0bd25edefb4db65c92eceebf
Stephen Haberman <stephen.haberman@gmail.com>
2023-12-15 20:34:05 -0600

    Add find test.

--- packages/tests/uuid-ids/src/entities/EntityManager.uuid.find.test.ts
+++ packages/tests/uuid-ids/src/entities/EntityManager.uuid.find.test.ts
@@ -1,9 +1,25 @@
 import { newAuthor } from "@src/entities/Author.factories";
 import { Book } from "@src/entities/Book";
-import { BookStatus } from "@src/entities/BookStatus";
-import { newEntityManager } from "@src/setupDbTests";
+import { BookStatus, BookStatusDetails } from "@src/entities/BookStatus";
+import { insertAuthor } from "@src/entities/inserts";
+import { insert, newEntityManager } from "@src/setupDbTests";
 
 describe("EntityManager.uuid.find", () => {
+  it("can find by uuid enums", async () => {
+    await insertAuthor({ first_name: "a1" });
+    await insert("books", {
+      id: "00000000-0000-0000-0000-000000000001",
+      status_id: BookStatusDetails.Published.id,
+      author_id: "20000000-0000-0000-0000-000000000000",
+      title: "b1",
+      created_at: new Date(),
+      updated_at: new Date(),
+    });
+    const em = newEntityManager();
+    const books = await em.find(Book, { status: BookStatus.Published });
+    expect(books.length).toBe(1);
+  });
+
   it("does not fail on new entities", async () => {
     const em = newEntityManager();
     const a1 = newAuthor(em);

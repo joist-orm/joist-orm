8d4887edfcb3ecec41daa6debd64829fdfde7bd8
Stephen Haberman <stephen.haberman@gmail.com>
2024-01-14 12:09:14 -0600

    Add prompts.

--- packages/codegen/src/codemods/index.ts
+++ packages/codegen/src/codemods/index.ts
@@ -1,4 +1,5 @@
 import fastglob from "fast-glob";
+import inquirer from "inquirer";
 import { run as jscodeshift } from "jscodeshift/src/Runner";
 import path from "path";
 import semver from "semver";
@@ -6,33 +7,67 @@ import { maybeAdjustForLocalDevelopment } from "../adjustVersion";
 import { Config } from "../config";
 
 export async function maybeRunTransforms(config: Config): Promise<void> {
-  const prevVersion = config.version;
-  const thisVersion = maybeAdjustForLocalDevelopment(require("../package.json").version);
-  await runTransforms(prevVersion, thisVersion);
-  // config.version = thisVersion;
-}
-
-export async function runTransforms(prevVersion: string, thisVersion: string): Promise<void> {
-  console.log({ prevVersion, thisVersion });
-  if (!semver.eq(prevVersion, thisVersion)) {
-    const todo = transforms.filter((t) => semver.lt(prevVersion, t.version));
-    for await (const t of todo) {
-      console.log(`Running ${t.name}`);
-      const transformPath = path.resolve(`${__dirname}/${t.name}.js`);
-      const paths = await fastglob(t.glob);
-      console.log(`Found ${transformPath} and ${paths.length} files`);
-      const res = await jscodeshift(transformPath, paths, { verbose: true, parser: "ts" });
-      console.log(res);
-    }
+  const confVersion = config.version;
+  const thisVersion = maybeAdjustForLocalDevelopment(require("../../package.json").version);
+  if (semver.eq(confVersion, thisVersion)) {
+    return;
   }
+
+  const todo = findPotentialTransforms(config, confVersion);
+  if (todo.length === 0) {
+    // Nothing to do, but bump the version anyway
+    config.version = thisVersion;
+    return;
+  }
+
+  console.log(
+    `Your project is on Joist ${confVersion} and there are ${todo.length} codemods to help upgrade to ${thisVersion}.`,
+  );
+
+  const { run } = await inquirer.prompt([{ name: "run", type: "confirm", message: `Would you like to run them?` }]);
+
+  // They opted out
+  if (!run) {
+    config.version = thisVersion;
+    return;
+  }
+
+  // Otherwise run them
+  for await (const t of todo) {
+    const { run } = await inquirer.prompt([
+      {
+        name: "run",
+        type: "confirm",
+        message: `Do you want to run ${t.description}?`,
+      },
+    ]);
+
+    const transformPath = path.resolve(`${__dirname}/${t.name}.js`);
+    const paths = await fastglob(t.glob);
+    console.log(`Running ${transformPath} against ${paths.length} files`);
+    console.log(`There will be a lot of jscodeshift output after this...\n\n\n`);
+    const res = await jscodeshift(transformPath, paths, {
+      // verbose: true,
+      parser: "ts",
+    });
+  }
+
+  console.log(`\n\n\nYou've been upgraded to ${thisVersion}!`);
+
+  // Now that all codemods they wanted to run have passed, bump the version
+  config.version = thisVersion;
 }
 
-type Codemod = { glob: string; version: string; name: string };
+function findPotentialTransforms(config: Config, prevVersion: string): Codemod[] {
+  const transforms: Codemod[] = [
+    {
+      version: "1.143.0",
+      glob: `${config.entitiesDirectory}/*.ts`,
+      name: "v1_143_0_rename_derived_async_property",
+      description: "Rename `hasPersistedAsyncProperty` to `hasReactiveField`",
+    },
+  ];
+  return transforms.filter((t) => semver.lt(prevVersion, t.version));
+}
 
-const transforms: Codemod[] = [
-  {
-    version: "1.143.0",
-    glob: "src/entities/*.ts",
-    name: "v1_143_0_rename_derived_async_property",
-  },
-];
+type Codemod = { glob: string; version: string; name: string; description: string };

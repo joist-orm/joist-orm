a7ba6a42c5156a64063350c15bfd4027cfa933f2
Stephen Haberman <stephen.haberman@gmail.com>
2024-01-14 13:10:44 -0600

    Exclude the test fixtures from tsconfig.

--- packages/codegen/src/codemods/__testfixtures__/v1_143_0_rename_derived_async_property.input.ts
+++ packages/codegen/src/codemods/__testfixtures__/v1_143_0_rename_derived_async_property.input.ts
@@ -1,7 +1,5 @@
-// @ts-ignore
 import { hasPersistedAsyncProperty, PersistedAsyncProperty } from "joist-orm";
 
 class Author {
-  // @ts-ignore
   count: PersistedAsyncProperty<Author, void> = hasPersistedAsyncProperty({ books: "reviews" }, () => {});
 }
--- packages/codegen/src/codemods/__testfixtures__/v1_143_0_rename_derived_async_property.output.ts
+++ packages/codegen/src/codemods/__testfixtures__/v1_143_0_rename_derived_async_property.output.ts
@@ -1,7 +1,5 @@
-// @ts-ignore
 import { hasReactiveField, ReactiveField } from "joist-orm";
 
 class Author {
-  // @ts-ignore
   count: ReactiveField<Author, void> = hasReactiveField({ books: "reviews" }, () => {});
 }

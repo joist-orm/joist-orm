dee98775a5a4e7d0fabb1ab67c45944cec07de9d
Stephen Haberman <stephen.haberman@gmail.com>
2024-01-14 13:45:58 -0600

    Santiy check that satisifies is supported.

--- packages/codegen/src/codemods/__testfixtures__/v1_143_0_rename_derived_async_property.output.ts
+++ packages/codegen/src/codemods/__testfixtures__/v1_143_0_rename_derived_async_property.output.ts
@@ -3,3 +3,6 @@ import { hasReactiveField, ReactiveField } from "joist-orm";
 class Author {
   count: ReactiveField<Author, void> = hasReactiveField({ books: "reviews" }, () => {});
 }
+
+// make sure we support this new syntax
+const a = 1 satisfies number;

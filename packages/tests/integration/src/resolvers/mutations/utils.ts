8de70a076b66f1747a35260ff5ce51800f361adf
Stephen Haberman <stephen.haberman@gmail.com>
2024-01-21 11:47:14 -0600

    Just use out-of-the-box saveEntities.

--- packages/tests/integration/src/resolvers/mutations/utils.ts
+++ packages/tests/integration/src/resolvers/mutations/utils.ts
@@ -1,16 +1,1 @@
-import { Context } from "@src/context";
-import { DeepPartialOrNull, Entity, EntityConstructor } from "joist-orm";
-
-export async function saveEntities<T extends Entity>(
-  ctx: Context,
-  type: EntityConstructor<T>,
-  inputs: DeepPartialOrNull<T>[],
-  opts: {
-    flush?: boolean;
-  } = {},
-): Promise<T[]> {
-  const { flush = true } = opts;
-  const entities = await Promise.all(inputs.map((input) => ctx.em.createOrUpdatePartial(type, input)));
-  if (flush) await ctx.em.flush();
-  return entities;
-}
+export { saveEntities } from "joist-graphql-resolver-utils";

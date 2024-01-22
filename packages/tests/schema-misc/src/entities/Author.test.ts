ad3c4b06b8b591fcb0887f1262781f3111fd1687
Stephen Haberman <stephen.haberman@gmail.com>
2024-01-21 22:12:39 -0600

    fix: Fix setter param names cannot be JS keywords.

--- packages/tests/schema-misc/src/entities/Author.test.ts
+++ packages/tests/schema-misc/src/entities/Author.test.ts
@@ -1,5 +1,5 @@
 import { newAuthor } from "@src/entities";
-import { newEntityManager, queries } from "@src/setupDbTests";
+import { newEntityManager, queries, resetQueryCount } from "@src/setupDbTests";
 
 describe("Author", () => {
   it.withCtx("can save", async () => {
@@ -13,12 +13,26 @@ describe("Author", () => {
     await em.flush();
     // Then we generate an insert
     expect(queries).toMatchInlineSnapshot(`
-      [
-        "BEGIN;",
-        "select nextval('authors_id_seq') from generate_series(1, 1)",
-        "INSERT INTO "authors" ("id", "firstName", "lastName", "createdAt", "updatedAt") VALUES ($1, $2, $3, $4, $5)",
-        "COMMIT;",
-      ]
+     [
+       "BEGIN;",
+       "select nextval('authors_id_seq') from generate_series(1, 1)",
+       "INSERT INTO "authors" ("id", "firstName", "lastName", "delete", "createdAt", "updatedAt") VALUES ($1, $2, $3, $4, $5, $6)",
+       "COMMIT;",
+     ]
+    `);
+
+    // And when we update it
+    resetQueryCount();
+    a.lastName = "updated";
+    a.delete = true;
+    await em.flush();
+    // Then we issued a valid SQL update
+    expect(queries).toMatchInlineSnapshot(`
+     [
+       "BEGIN;",
+       "WITH data (id, "lastName", "delete", "updatedAt", __original_updated_at) AS (VALUES ($1::int, $2::character varying, $3::boolean, $4::timestamp with time zone, $5::timestamptz) ) UPDATE authors SET "lastName" = data."lastName", "delete" = data."delete", "updatedAt" = data."updatedAt" FROM data WHERE authors.id = data.id AND date_trunc('milliseconds', authors."updatedAt") = data.__original_updated_at RETURNING authors.id",
+       "COMMIT;",
+     ]
     `);
   });
 });
--- packages/tests/schema-misc/src/entities/Book.test.ts
+++ packages/tests/schema-misc/src/entities/Book.test.ts
@@ -15,13 +15,13 @@ describe("Book", () => {
     // And after we flush
     await em.flush();
     expect(queries).toMatchInlineSnapshot(`
-      [
-        "BEGIN;",
-        "select nextval('authors_id_seq') from generate_series(1, 1) UNION ALL select nextval('book_id_seq') from generate_series(1, 1)",
-        "INSERT INTO "authors" ("id", "firstName", "lastName", "createdAt", "updatedAt") VALUES ($1, $2, $3, $4, $5)",
-        "INSERT INTO "book" ("id", "title", "authorId") VALUES ($1, $2, $3)",
-        "COMMIT;",
-      ]
+     [
+       "BEGIN;",
+       "select nextval('authors_id_seq') from generate_series(1, 1) UNION ALL select nextval('book_id_seq') from generate_series(1, 1)",
+       "INSERT INTO "authors" ("id", "firstName", "lastName", "delete", "createdAt", "updatedAt") VALUES ($1, $2, $3, $4, $5, $6)",
+       "INSERT INTO "book" ("id", "title", "authorId") VALUES ($1, $2, $3)",
+       "COMMIT;",
+     ]
     `);
     // Then we still don't see any values
     expect(getOrmField(b).data).toEqual({

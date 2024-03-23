a221069528608a745f492a233a5d3e1385df483e
Stephen Haberman <stephen.haberman@gmail.com>
2024-03-22 21:18:23 -0500

    Fix tests.

--- packages/tests/integration/src/entities/Book.test.ts
+++ packages/tests/integration/src/entities/Book.test.ts
@@ -15,7 +15,7 @@ describe("Book", () => {
     const em = newEntityManager();
     const a1 = em.create(Author, { firstName: "a1" });
     const b1 = em.create(Book, { title: "b1", author: a1 });
-    expect(b1.order).toEqual(1);
+    expect(b1.notes).toEqual("Notes for b1");
   });
 
   it("does not reset default values on load", async () => {

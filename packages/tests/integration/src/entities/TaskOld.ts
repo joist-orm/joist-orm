eba4b7f15544ed7892535315dd5dfd9a8dadb77d
Stephen Haberman <stephen.haberman@gmail.com>
2024-03-26 21:02:06 -0500

    fix: Fix subtypes not running reactive rules.

--- packages/tests/integration/src/entities/TaskOld.ts
+++ packages/tests/integration/src/entities/TaskOld.ts
@@ -1,13 +1,18 @@
-import { TaskOldCodegen } from "./entities";
-import { taskOldConfig as config } from "./entities";
+import { TaskOldCodegen, taskOldConfig as config } from "./entities";
 
 export class TaskOld extends TaskOldCodegen {
   transientFields = {
-    oldHookRan: false,
+    oldSimpleRuleRan: false,
+    oldReactiveRuleRan: false,
   };
 }
 
+/** For testing that STI reactive rules run correctly. */
+config.addRule("deletedAt", (t) => {
+  t.transientFields.oldReactiveRuleRan = true;
+});
+
 /** For testing that STI subtype hooks run correctly. */
 config.addRule((t) => {
-  t.transientFields.oldHookRan = true;
+  t.transientFields.oldSimpleRuleRan = true;
 });

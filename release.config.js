fc91e719fb147f28ea4b348588f549318a5de60d
Stephen Haberman <stephen.haberman@gmail.com>
2024-04-14 12:29:24 -0500

    fix: Fix hot reloading in Next 14 (#1038)
    * fix: Update NextJS check for better global var.
    
    * Include a few ci fixes.

--- release.config.js
+++ release.config.js
@@ -8,7 +8,7 @@ module.exports = {
       "@semantic-release/exec",
       {
         prepareCmd: "./set-version.sh ${nextRelease.version}",
-        publishCmd: "yarn workspaces foreach --all --no-private npm publish",
+        publishCmd: "yarn workspaces foreach -v --all --no-private npm publish --tolerate-republish",
       },
     ],
     "@semantic-release/github",

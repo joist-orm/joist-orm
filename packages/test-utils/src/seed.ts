c146704018f46358b41337ec70df27f9d098ab7d
Stephen Haberman <stephen.haberman@gmail.com>
2024-04-14 20:15:48 -0500

    Add comments.

--- packages/test-utils/src/seed.ts
+++ packages/test-utils/src/seed.ts
@@ -1,12 +1,29 @@
 import { EntityManager, newPgConnectionConfig, PostgresDriver } from "joist-orm";
 import { knex as createKnex } from "knex";
 
+/**
+ * Allows easily seeding your local database with test data from the factories.
+ *
+ * This will delete all data in the database, so is restricted to only `local` and
+ * `test` `NODE_ENV`s.
+ *
+ * We currently make a lot of assumptions:
+ *
+ * - That the project is using knex (all Joist projects do atm)
+ * - That the project is using postgres (all Joist projects do atm)
+ * - That the project does not have a context, or at least does not require a context to seed
+ *
+ * If any of these assumptions are not true, you can copy this code into your project.
+ */
 export function seed<E extends EntityManager = EntityManager>(fn: (em: E) => Promise<void>): void {
   const env = process.env.NODE_ENV;
   if (env !== "local" && env !== "test") {
     throw new Error("seed will only run with NODE_ENV=local or NODE_ENV=test because it resets the database");
   }
 
+  // We make a lot of assumptions about the project is surely using
+  // knex & postgres, but at some point these will be configurable...
+  // Maybe we can detect it in the `DATABASE_URL` or what not.
   const knex = createKnex({ client: "pg", connection: newPgConnectionConfig() });
 
   async function seed() {

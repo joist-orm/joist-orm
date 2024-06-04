cc95fbe123485ac1bf5553a21c97cec98b8e3ecc
Stephen Haberman <stephen.haberman@gmail.com>
2024-06-03 21:10:40 -0500

    fix: Add compile error for non-fields using newRequiredRule.

--- packages/tests/integration/src/rules.test.ts
+++ packages/tests/integration/src/rules.test.ts
@@ -1,7 +1,15 @@
-import { ValidationErrors } from "joist-orm";
+import { newRequiredRule, ValidationErrors } from "joist-orm";
+import { Author } from "src/entities";
 
 describe("ValidationErrors", () => {
   it("serialises even with prototype lost", () => {
     expect(JSON.stringify({ ...new ValidationErrors("example") })).toEqual(`"ValidationErrors: example"`);
+  });
+
+  describe("newRequiredRule", () => {
+    it("cannot be used on o2o fields", () => {
+      // @ts-expect-error
+      newRequiredRule<Author>("image");
+    });
   });
 });

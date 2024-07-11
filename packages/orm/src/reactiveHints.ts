a802d7cea4dc7285a632ddf930611333b5728810
Zachary Gavin <zgavin@homebound.com>
2024-07-11 12:20:08 -0600

    also included reactive getter support and added comments

--- packages/orm/src/reactiveHints.ts
+++ packages/orm/src/reactiveHints.ts
@@ -239,6 +239,9 @@ export function reverseReactiveHint<T extends Entity>(
       // notice their primitive values changing, and kicking off any downstream reactive fields as necessary.
       const p = getProperties(meta)[key];
       if (p instanceof AsyncPropertyImpl) {
+        // if the field is marked as readonly, then we can assume that applies to its entire hint as well and can
+        // simply omit it.  This also allows us to use non-reactive async props as long as they are readonly.
+        if (isReadOnly) return [];
         if (!p.reactiveHint) {
           throw new Error(
             `AsyncProperty ${key} cannot be used in reactive hints in ${rootType.name}.ts hint ${JSON.stringify(
@@ -246,8 +249,11 @@ export function reverseReactiveHint<T extends Entity>(
             )}, please use hasReactiveAsyncProperty instead`,
           );
         }
-        return isReadOnly ? [] : reverseReactiveHint(rootType, meta.cstr, p.reactiveHint, undefined, false);
+        return reverseReactiveHint(rootType, meta.cstr, p.reactiveHint, undefined, false);
       } else if (p instanceof ReactiveGetterImpl) {
+        // if the field is marked as readonly, then we can assume that applies to its entire hint as well and can
+        // simply omit it
+        if (isReadOnly) return [];
         return reverseReactiveHint(rootType, meta.cstr, p.reactiveHint, undefined, false);
       } else {
         throw new Error(`Invalid hint in ${rootType.name}.ts ${JSON.stringify(hint)}`);

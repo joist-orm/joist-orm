9ca67a83a6ddba9004dae9336a79a2c26f2d2902
Adam Gray <881622+acgray@users.noreply.github.com>
2024-07-28 17:19:16 -0400

    Revert "return connection string directly if specified"
    This reverts commit 418aed0b3aaa67bc320c512d54a6e26fa4606a09.

--- packages/utils/src/connection.test.ts
+++ packages/utils/src/connection.test.ts
@@ -2,9 +2,14 @@ import { newPgConnectionConfig } from "./connection";
 
 describe("connection", () => {
   it("should parse single DATABASE_URL", () => {
-    const url = "postgres://joist:local@db:5432/joist";
-    const info = newPgConnectionConfig({ DATABASE_URL: url });
-    expect(info).toEqual(url);
+    const info = newPgConnectionConfig({ DATABASE_URL: "postgres://joist:local@db:5432/joist" });
+    expect(info).toEqual({
+      database: "joist",
+      host: "db",
+      password: "local",
+      port: 5432,
+      user: "joist",
+    });
   });
 
   it("should parse multiple DB variables", () => {

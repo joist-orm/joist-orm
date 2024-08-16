608c83af653d95e1809626f0b7409bcbcf5a13b0
Stephen Haberman <stephen.haberman@gmail.com>
2024-08-16 18:02:14 -0500

    Failing test.

--- packages/tests/integration/src/FieldLogging.test.ts
+++ packages/tests/integration/src/FieldLogging.test.ts
@@ -1,7 +1,7 @@
 import ansiRegex = require("ansi-regex");
 import { FieldLogger } from "joist-orm";
-import { Author } from "src/entities";
-import { insertAuthor } from "src/entities/inserts";
+import { Author, Publisher } from "src/entities";
+import { insertAuthor, insertPublisher } from "src/entities/inserts";
 import { newEntityManager } from "src/testEm";
 
 let fieldOutput: string[] = [];
@@ -25,6 +25,17 @@ describe("FieldLogging", () => {
     const a1 = await em.load(Author, "1");
     a1.lastName = undefined;
     expect(fieldOutput[0]).toMatch(/a:1.lastName = undefined at FieldLogging.test.ts:(\d+)↩/);
+  });
+
+  it("sees m2o sets", async () => {
+    await insertPublisher({ name: "p1" });
+    await insertAuthor({ first_name: "a1" });
+    const em = newEntityManager();
+    em.setFieldLogging(new StubFieldLogger());
+    const p1 = await em.load(Publisher, "p:1");
+    const a1 = await em.load(Author, "a:1");
+    a1.publisher.set(p1);
+    expect(fieldOutput[0]).toMatch(/a:1.publisher = p:1 at FieldLogging.test.ts:(\d+)↩/);
   });
 });
 

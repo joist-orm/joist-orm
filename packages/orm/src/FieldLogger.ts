b8672b0fa8b4067ef11ff43fc7e7ded1dbe9f299
Stephen Haberman <stephen.haberman@gmail.com>
2024-08-17 17:03:49 -0500

    Add filtering and breakpoint support.

--- packages/orm/src/FieldLogger.ts
+++ packages/orm/src/FieldLogger.ts
@@ -2,24 +2,41 @@ import ansis from "ansis";
 import { Entity, isEntity } from "./Entity";
 import { getFuzzyCallerName } from "./config";
 
-const { gray, green, yellow, white, blue, red } = ansis;
+const { gray, green, yellow, blue, red } = ansis;
 let globalLogger: FieldLogger | undefined = undefined;
 type WriteFn = (line: string) => void;
+
+export type FieldLoggerWatch = {
+  /** The entity name, i.e. `Author` */
+  entity: string;
+  /** The fields, i.e. `["firstName", "lastName"]`. */
+  fieldNames?: string[];
+  /** Whether to stop a debugger when the field is set. */
+  breakpoint?: boolean;
+};
 
 /**
  * Logs the setting of fields on entities to a `writeFn`, which defaults to `process.stdout`.
  */
 export class FieldLogger {
-  private writeFn: WriteFn;
+  readonly #writeFn: WriteFn;
+  readonly #watching: FieldLoggerWatch[] = [];
 
-  // We default to process.stdout.write to side-step around Jest's console.log instrumentation
-  constructor(writeFn: WriteFn = process.stdout.write.bind(process.stdout)) {
-    this.writeFn = writeFn;
+  constructor(
+    /** The fields to watch, defaults to all fields. */
+    watching: FieldLoggerWatch[],
+    /** An optional/semi-internal write function, mostly for testing. */
+    writeFn?: WriteFn,
+  ) {
+    // We default to process.stdout.write to side-step around Jest's console.log instrumentation
+    this.#writeFn = writeFn ?? process.stdout.write.bind(process.stdout);
+    this.#watching = watching ?? [];
   }
 
   logSet(entity: Entity, fieldName: string, value: unknown): void {
-    const color = typeof value === "boolean" ? red : isEntity(value) ? green : blue;
-
+    const log = this.shouldLog(entity, fieldName);
+    if (!log) return;
+    const color = this.getColor(value);
     this.log(
       green.bold(`${entity.toTaggedString()}`) + yellow(`.${fieldName}`),
       gray(`=`),
@@ -28,9 +45,29 @@ export class FieldLogger {
       // to really get "the caller's location", so use `getFuzzyCallerName` instead of `getCallerName`.
       gray(`at ${getFuzzyCallerName()}`),
     );
+    if (log === "breakpoint") debugger;
   }
 
   private log(...line: string[]): void {
-    this.writeFn(`${line.join(" ")}\n`);
+    this.#writeFn(`${line.join(" ")}\n`);
+  }
+
+  private shouldLog(entity: Entity, fieldName: string): boolean | "breakpoint" {
+    if (this.#watching.length === 0) return true;
+    for (const { entity: entityName, fieldNames, breakpoint } of this.#watching) {
+      if (entity.constructor.name === entityName && (fieldNames === undefined || fieldNames.includes(fieldName))) {
+        return breakpoint ? "breakpoint" : true;
+      }
+    }
+    return false;
+  }
+
+  // Do some `typeof` -> color
+  private getColor(value: unknown): typeof red {
+    return typeof value === "boolean"
+      ? red // boolean => red
+      : isEntity(value)
+        ? green // entity => green (matches other logging)
+        : blue; // otherwise strings/dates/numbers are blue;
   }
 }
--- packages/tests/integration/src/FieldLogging.test.ts
+++ packages/tests/integration/src/FieldLogging.test.ts
@@ -1,6 +1,6 @@
 import ansiRegex = require("ansi-regex");
-import { FieldLogger } from "joist-orm";
-import { Author, Publisher } from "src/entities";
+import { FieldLogger, FieldLoggerWatch } from "joist-orm";
+import { Author, newBook, Publisher } from "src/entities";
 import { insertAuthor, insertPublisher } from "src/entities/inserts";
 import { newEntityManager } from "src/testEm";
 
@@ -37,6 +37,65 @@ describe("FieldLogging", () => {
     a1.publisher.set(p1);
     expect(fieldOutput[0]).toMatch(/a:1.publisher = p:1 at FieldLogging.test.ts:(\d+)↩/);
   });
+
+  it("sees all fields by default", async () => {
+    const em = newEntityManager();
+    em.setFieldLogging(new StubFieldLogger());
+    newBook(em);
+    expect(fieldOutput).toMatchInlineSnapshot(`
+     [
+       "a#1.firstName = a1 at newAuthor.ts:13↩",
+       "a#1.age = 40 at newAuthor.ts:13↩",
+       "b#1.title = title at newBook.ts:9↩",
+       "b#1.order = 1 at newBook.ts:9↩",
+       "b#1.author = Author#1 at newBook.ts:9↩",
+       "b#1.notes = Notes for title at defaults.ts:28↩",
+     ]
+    `);
+  });
+
+  it("can filter fields by entity", async () => {
+    const em = newEntityManager();
+    em.setFieldLogging(new StubFieldLogger([{ entity: "Author" }]));
+    newBook(em);
+    expect(fieldOutput).toMatchInlineSnapshot(`
+     [
+       "a#1.firstName = a1 at newAuthor.ts:13↩",
+       "a#1.age = 40 at newAuthor.ts:13↩",
+     ]
+    `);
+  });
+
+  it("can filter fields by entity and fieldName", async () => {
+    const em = newEntityManager();
+    em.setFieldLogging(new StubFieldLogger([{ entity: "Author", fieldNames: ["age"] }]));
+    newBook(em);
+    expect(fieldOutput).toMatchInlineSnapshot(`
+     [
+       "a#1.age = 40 at newAuthor.ts:13↩",
+     ]
+    `);
+  });
+
+  // skipped because this needs to run in debug mode
+  it("can hit a breakpoint", async () => {
+    const em = newEntityManager();
+    em.setFieldLogging(
+      new StubFieldLogger([
+        {
+          entity: "Author",
+          fieldNames: ["age"],
+          breakpoint: true,
+        },
+      ]),
+    );
+    newBook(em);
+    expect(fieldOutput).toMatchInlineSnapshot(`
+     [
+       "a#1.age = 40 at newAuthor.ts:13↩",
+     ]
+    `);
+  });
 });
 
 beforeEach(() => {
@@ -44,10 +103,11 @@ beforeEach(() => {
 });
 
 class StubFieldLogger extends FieldLogger {
-  constructor() {
-    super();
-    // super((line: string) => {
-    //   fieldOutput.push(line.replace(ansiRegex(), "").replace("\n", "↩"));
-    // });
+  constructor(watching: FieldLoggerWatch[] = []) {
+    // uncomment to see colorized output
+    // super();
+    super(watching, (line: string) => {
+      fieldOutput.push(line.replace(ansiRegex(), "").replace("\n", "↩"));
+    });
   }
 }

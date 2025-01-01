4a450d482549ff55de0a60b38f363d6b87bce562
Stephen Haberman <stephen.haberman@gmail.com>
2025-01-01 09:42:14 -0600

    Fix tests.

--- packages/tests/integration/src/setupTestEnv.ts
+++ packages/tests/integration/src/setupTestEnv.ts
@@ -2,7 +2,7 @@ import { GetEnvVars } from "env-cmd";
 
 export default async function globalSetup() {
   process.env.TZ = "UTC";
-  process.env.DEBUG = "knex:query,knex:bindings";
+  // process.env.DEBUG = "knex:query,knex:bindings";
   // process.env.DEBUG = "knex:*";
   Object.entries(await GetEnvVars()).forEach(([key, value]) => (process.env[key] = value));
 }

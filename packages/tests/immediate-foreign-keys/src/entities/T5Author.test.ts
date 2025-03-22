29b7cd6b3d71cb5448fcee351d71575be5e20cb9
Stephen Haberman <stephen.haberman@gmail.com>
2025-03-22 11:37:32 -0500

    Fix snapshots.

--- packages/tests/immediate-foreign-keys/src/entities/T5Author.test.ts
+++ packages/tests/immediate-foreign-keys/src/entities/T5Author.test.ts
@@ -12,9 +12,9 @@ describe("T5Author", () => {
      [
        "begin ",
        "select nextval('t5_authors_id_seq') from generate_series(1, 1) UNION ALL select nextval('t5_books_id_seq') from generate_series(1, 2) UNION ALL select nextval('t5_book_reviews_id_seq') from generate_series(1, 2)",
-       "INSERT INTO "t5_authors" ("id", "first_name") VALUES (?, ?)",
-       "INSERT INTO "t5_books" ("id", "title", "author_id") VALUES (?, ?, ?),(?, ?, ?)",
-       "INSERT INTO "t5_book_reviews" ("id", "title", "book_id") VALUES (?, ?, ?),(?, ?, ?)",
+       "INSERT INTO t5_authors (id, first_name) SELECT unnest(?::int[]), unnest(?::character varying[])",
+       "INSERT INTO t5_books (id, title, author_id) SELECT unnest(?::int[]), unnest(?::character varying[]), unnest(?::int[])",
+       "INSERT INTO t5_book_reviews (id, title, book_id) SELECT unnest(?::int[]), unnest(?::character varying[]), unnest(?::int[])",
        "commit",
      ]
     `);
@@ -29,9 +29,9 @@ describe("T5Author", () => {
      [
        "begin ",
        "select nextval('t5_book_reviews_id_seq') from generate_series(1, 1) UNION ALL select nextval('t5_authors_id_seq') from generate_series(1, 1) UNION ALL select nextval('t5_books_id_seq') from generate_series(1, 1)",
-       "INSERT INTO "t5_authors" ("id", "first_name") VALUES (?, ?)",
-       "INSERT INTO "t5_books" ("id", "title", "author_id") VALUES (?, ?, ?)",
-       "INSERT INTO "t5_book_reviews" ("id", "title", "book_id") VALUES (?, ?, ?)",
+       "INSERT INTO t5_authors (id, first_name) SELECT unnest(?::int[]), unnest(?::character varying[])",
+       "INSERT INTO t5_books (id, title, author_id) SELECT unnest(?::int[]), unnest(?::character varying[]), unnest(?::int[])",
+       "INSERT INTO t5_book_reviews (id, title, book_id) SELECT unnest(?::int[]), unnest(?::character varying[]), unnest(?::int[])",
        "commit",
      ]
     `);

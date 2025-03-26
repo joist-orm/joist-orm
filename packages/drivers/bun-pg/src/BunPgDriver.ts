e647b62865c07fb9f0fb456c7e898ba91e60323e
Stephen Haberman <stephen.haberman@gmail.com>
2025-03-26 07:21:40 -0500

    Combine flushEntities and flushJoinRows to pipeline.

--- packages/drivers/bun-pg/src/BunPgDriver.ts
+++ packages/drivers/bun-pg/src/BunPgDriver.ts
@@ -60,7 +60,7 @@ export class BunPgDriver implements Driver<TransactionSQL> {
     throw new Error("Method not implemented.");
   }
 
-  async flushEntities(em: EntityManager, todos: Record<string, Todo>): Promise<void> {
+  async flush(em: EntityManager, todos: Record<string, Todo>, joinRows: Record<string, JoinRowTodo>): Promise<void> {
     const sql = (em.txn ?? fail("Expected EntityManager.txn to be set")) as TransactionSQL;
     await this.#idAssigner.assignNewIds(todos);
     const ops = generateOps(todos);
@@ -71,10 +71,6 @@ export class BunPgDriver implements Driver<TransactionSQL> {
       ...ops.updates.map((op) => batchUpdate(sql, op)),
       ...ops.deletes.map((op) => batchDelete(sql, op)),
     ]);
-  }
-
-  flushJoinTables(em: EntityManager, joinRows: Record<string, JoinRowTodo>): Promise<void> {
-    throw new Error("Method not implemented.");
   }
 }
 

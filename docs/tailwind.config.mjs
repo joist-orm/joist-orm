08731ae81cd004fd2d9c1f8d7068a31d79de0f3f
Stephen Haberman <stephen.haberman@gmail.com>
2025-05-06 22:09:52 -0500

    docs: Use better fonts. (#1488)
    Inter Var for body/heading, and JetBrains Mono for code.
    
    Ngl taking a cue from Atlassian Confluence.

--- docs/tailwind.config.mjs
+++ docs/tailwind.config.mjs
@@ -3,6 +3,15 @@ import starlightPlugin from "@astrojs/starlight-tailwind";
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', 'Inter', 'sans-serif'],
+        heading: ['Inter var', 'Inter', 'sans-serif'],
+        mono: ['JetBrains Mono', 'monospace'],
+      },
+    },
+  },
   plugins: [starlightPlugin()],
   // require('@tailwindcss/typography'),
 };

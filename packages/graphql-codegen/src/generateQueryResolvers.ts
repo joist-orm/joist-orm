4dd6a9c2a7e3b76ecf85a992693a5491e5942ecb
Ben Limmer <ben@benlimmer.com>
2025-10-03 13:06:20 -0600

    fix: more import updates

--- packages/graphql-codegen/src/generateQueryResolvers.ts
+++ packages/graphql-codegen/src/generateQueryResolvers.ts
@@ -1,18 +1,19 @@
 import { camelCase, sentenceCase } from "change-case";
-import { DbMetadata } from "joist-codegen";
+import { DbMetadata, type Config } from "joist-codegen";
 import { CodegenFile, code, imp } from "ts-poet";
+import { getEntitiesImportPath } from "./utils";
 
-const queryResolvers = imp("t:QueryResolvers@src/generated/graphql-types");
-const makeRunResolver = imp("makeRunInputMutation@src/resolvers/testUtils");
+const queryResolvers = imp("t:QueryResolvers@src/generated/graphql-types.ts");
+const makeRunResolver = imp("makeRunInputMutation@src/resolvers/testUtils.ts");
 
 /**
  * Generates top-level query resolvers.
  */
-export function generateQueryResolvers(db: DbMetadata): CodegenFile[] {
+export function generateQueryResolvers(config: Config, db: DbMetadata): CodegenFile[] {
+  const entitiesPath = getEntitiesImportPath(config);
   const resolvers = db.entities.map((e) => {
     const { name } = e;
     const camelName = camelCase(name);
-    const type = imp(`${name}@src/entities`);
     return {
       name: `resolvers/${camelName}/${camelName}Query.ts`,
       overwrite: false,
@@ -29,14 +30,14 @@ export function generateQueryResolvers(db: DbMetadata): CodegenFile[] {
   const testFiles = db.entities.map((e) => {
     const { name, tagName } = e;
     const camelName = camelCase(name);
-    const resolverConst = imp(`${camelName}@src/resolvers/${camelName}/${camelName}Query`);
+    const resolverConst = imp(`${camelName}@src/resolvers/${camelName}/${camelName}Query.ts`);
     return {
       name: `resolvers/${camelName}/${camelName}Query.test.ts`,
       overwrite: false,
       contents: code`
         describe("${camelName}", () => {
           it.withCtx("returns a ${sentenceCase(name)}", async (ctx) => {
-            const ${tagName} = ${imp(`new${name}@src/entities`)}(ctx.em);
+            const ${tagName} = ${imp(`new${name}@${entitiesPath}`)}(ctx.em);
             const result = await run(ctx, {}, "${camelName}", () => ({ id: ${tagName}.id }));
             expect(result).toMatchEntity(${tagName});
           });

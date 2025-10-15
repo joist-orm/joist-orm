8d05918b8df928100a7dcecbe32c4cabbc85c645
Zachary Gavin <zgavin@gmail.com>
2025-10-14 20:27:34 -0600

    feat: add the ability to remove conditions to visitConditions

--- packages/orm/src/QueryVisitor.ts
+++ packages/orm/src/QueryVisitor.ts
@@ -11,7 +11,7 @@ import { assertNever } from "./utils";
 interface Visitor {
   visitExp?(c: ParsedExpressionFilter): ParsedExpressionFilter | void;
   visitRaw?(c: RawCondition): RawCondition | ParsedExpressionFilter | void;
-  visitCond(c: ColumnCondition): ColumnCondition | ParsedExpressionFilter | RawCondition | void;
+  visitCond(c: ColumnCondition): ColumnCondition | ParsedExpressionFilter | RawCondition | null | void;
 }
 
 /**
@@ -19,6 +19,8 @@ interface Visitor {
  *
  * If the visitors return a new condition or expression, the returned expression
  * will replace the original one at that location in the expression.
+ *
+ * If the visitor returns null, the condition or expression will be removed from the tree.
  */
 export function visitConditions(query: ParsedFindQuery, visitor: Visitor): void {
   const todo = [query];
@@ -35,33 +37,45 @@ export function visitConditions(query: ParsedFindQuery, visitor: Visitor): void 
 
 export function visitFilter(pc: ParsedExpressionCondition, visitor: Visitor) {
   if (pc.kind === "exp") {
+    const toRemove = new Set<number>();
     pc.conditions.forEach((c, i) => {
       if (c.kind === "column") {
         const result = visitor.visitCond(c);
-        if (result) {
+        if (result === null) {
+          toRemove.add(i);
+        } else if (result) {
           pc.conditions[i] = result;
         }
       } else if (c.kind === "exp") {
         const result = visitor.visitExp?.(c);
-        if (result) pc.conditions[i] = result;
+        if (result === null) {
+          toRemove.add(i);
+          return;
+        } else if (result) {
+          pc.conditions[i] = result;
+        }
         visitFilter(result ?? c, visitor);
       } else if (c.kind === "raw") {
         const result = visitor.visitRaw?.(c);
-        if (result) {
+        if (result === null) {
+          toRemove.add(i);
+        } else if (result) {
           pc.conditions[i] = result;
         }
       } else {
         assertNever(c);
       }
     });
+    // iterate backwards so we don't mess up the indices
+    if (toRemove.size > 0) pc.conditions = pc.conditions.filter((_, i) => !toRemove.has(i));
   } else if (pc.kind === "column") {
     const result = visitor.visitCond(pc);
-    if (result) {
+    if (result || result === null) {
       throw new Error("ParsedExpressionCondition overload not support mutating the condition");
     }
   } else if (pc.kind === "raw") {
     const result = visitor.visitRaw?.(pc);
-    if (result) {
+    if (result || result === null) {
       throw new Error("ParsedExpressionCondition overload not support mutating the condition");
     }
   } else {

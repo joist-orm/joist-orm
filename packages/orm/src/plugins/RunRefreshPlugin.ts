f69a29ba7a078ce4a4ba753f4378551ed625b568
Zachary Gavin <zgavin@homebound.com>
2025-11-12 12:29:08 -0700

    more

--- packages/orm/src/plugins/RunRefreshPlugin.ts
+++ packages/orm/src/plugins/RunRefreshPlugin.ts
@@ -2,6 +2,7 @@ import { getMetadataForType } from "../configure";
 import { loadDataLoader } from "../dataloaders/loadDataLoader";
 import { Entity } from "../Entity";
 import { EntityManager, getEmInternalApi, isDefined } from "../EntityManager";
+import { getRelations } from "../index";
 import { Plugin } from "../PluginManager";
 import { ManyToManyCollection } from "../relations";
 import { JoinRowTodo, Todo } from "../Todo";
@@ -32,11 +33,17 @@ export class RunRefreshPlugin extends Plugin {
     api.setIsRefreshing(true);
     api.clearDataloaders();
     api.clearPreloadedRelations();
-
     await Promise.all([
       ...Object.entries(this.#entities).flatMap(([type, entities]) => {
         const meta = getMetadataForType(type);
-        return entities.values().map((e) => loadDataLoader(em, meta, true).load({ entity: e.idTagged, hint: [] }));
+        return (
+          entities
+            .values()
+            // We only need to refresh entities actually in the old em. If we created entities in the new em they'll
+            // either get loaded via relation loads, mapResultToOriginalEm, or the old em doesn't care about them.
+            .filter((e) => em.findExistingInstance(e.idTagged))
+            .map((e) => loadDataLoader(em, meta, true).load({ entity: e.idTagged, hint: [] }))
+        );
       }),
       ...Object.entries(this.#joins).flatMap(([joinTable, entities]) => {
         let relations: string[];
@@ -46,14 +53,17 @@ export class RunRefreshPlugin extends Plugin {
           .filter(isDefined)
           .flatMap((e) => {
             if (!relations) {
-              relations = [];
+              const m2m = getRelations(e).find(
+                (r) => r instanceof ManyToManyCollection && r.joinTableName === joinTable,
+              )! as ManyToManyCollection<any, any>;
+              relations = [m2m.fieldName, m2m.otherFieldName];
             }
             return relations
               .values()
-              .map((relation) =>
-                relation in e ? ((e as any)[relation] as ManyToManyCollection<any, any>).load() : undefined,
-              )
-              .filter(isDefined);
+              .map((relation) => (relation in e ? ((e as any)[relation] as ManyToManyCollection<any, any>) : undefined))
+              .filter(isDefined)
+              .filter((r) => r.isLoaded)
+              .map((r) => r.load());
           });
       }),
     ]);

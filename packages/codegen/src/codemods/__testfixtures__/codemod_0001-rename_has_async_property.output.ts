7b2d7d7cbca76092c32b669fa1eecc2726d98f0e
Stephen Haberman <stephen.haberman@gmail.com>
2026-04-25 10:21:28 -0500

    fix: Fix bug in AsyncProperty rename codemod. (#1794)

--- packages/codegen/src/codemods/__testfixtures__/codemod_0001-rename_has_async_property.output.ts
+++ packages/codegen/src/codemods/__testfixtures__/codemod_0001-rename_has_async_property.output.ts
@@ -7,6 +7,8 @@ import {
 } from "joist-core";
 
 class Author {
+  constructor() {}
+
   count: Property<Author, number> = hasProperty({ books: "title" }, (a) => a.books.get.length);
   reactive: Property<Author, number> = hasReactiveProperty({ books: "title" }, (a) => a.books.get.length);
 }
--- packages/codegen/src/codemods/codemod_0001-rename_has_async_property.ts
+++ packages/codegen/src/codemods/codemod_0001-rename_has_async_property.ts
@@ -8,23 +8,26 @@ export const codemod_0001_rename_has_async_property = new JscodeshiftMod(
   (config) => `${config.entitiesDirectory}/*.ts`,
 );
 
-const renames: Record<string, string> = {
-  hasAsyncProperty: "hasProperty",
-  hasReactiveAsyncProperty: "hasReactiveProperty",
-  AsyncProperty: "Property",
-  AsyncPropertyImpl: "PropertyImpl",
-  isAsyncProperty: "isProperty",
-  isLoadedAsyncProperty: "isLoadedProperty",
-};
+const renames = new Map<string, string>([
+  ["hasAsyncProperty", "hasProperty"],
+  ["hasReactiveAsyncProperty", "hasReactiveProperty"],
+  ["AsyncProperty", "Property"],
+  ["AsyncPropertyImpl", "PropertyImpl"],
+  ["isAsyncProperty", "isProperty"],
+  ["isLoadedAsyncProperty", "isLoadedProperty"],
+]);
 
 /** Find/replace the various `*AsyncProperty*` symbols with their `*Property*` counterparts. */
-const transform: Transform = (file, api) => {
+const transform: Transform = function (file, api) {
   const j = api.jscodeshift;
   return j(file.source)
     .find(j.Identifier)
-    .filter((path) => path.node.name in renames)
     .forEach((path) => {
-      j(path).replaceWith(j.identifier(renames[path.node.name]));
+      const rename = renames.get(path.node.name);
+      if (!rename) {
+        return;
+      }
+      j(path).replaceWith(j.identifier(rename));
     })
     .toSource();
 };

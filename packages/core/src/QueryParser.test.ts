d0e985df10eaf3d0fda56f46c09de8fcafcda916
Stephen Haberman <stephen.haberman@gmail.com>
2026-06-26 12:06:52 -0500

    fix: Add missing keys to ValueGraphQLFilter. (#1906)

--- packages/core/src/QueryParser.test.ts
+++ packages/core/src/QueryParser.test.ts
@@ -11,5 +11,18 @@ describe("QueryParser", () => {
       expect(parseValueFilter({ eq: false })).toEqual([{ kind: "eq", value: false }]);
       expect(parseValueFilter({ ne: false })).toEqual([{ kind: "ne", value: false }]);
     });
+
+    it("parses search filters", () => {
+      expect(parseValueFilter({ search: "foo bar" })).toEqual([{ kind: "ilike", value: "%foo%bar%" }]);
+    });
+
+    it("parses json path filters", () => {
+      expect(parseValueFilter({ pathExists: "$.tags[*] ? (@ == \"orm\")" })).toEqual([
+        { kind: "jsonPathExists", value: "$.tags[*] ? (@ == \"orm\")" },
+      ]);
+      expect(parseValueFilter({ pathIsTrue: "$.active == true" })).toEqual([
+        { kind: "jsonPathPredicate", value: "$.active == true" },
+      ]);
+    });
   });
 });

2c188e5a59516d5b88db99f1989157c40300f892
Stephen Haberman <stephen.haberman@gmail.com>
2026-06-29 19:37:49 -0500

    chore: Bump dependencies. (#1912)
    * chore: Bump dependencies.
    
    * Revert temporal-polyfill bump.

--- packages/graphql-codegen/src/history.ts
+++ packages/graphql-codegen/src/history.ts
@@ -1,11 +1,12 @@
 import { createFromBuffer } from "@dprint/formatter";
-import { getBuffer } from "@dprint/json";
+import { getPath } from "@dprint/json";
+import { readFileSync } from "fs";
 import { Fs, sortKeys } from "./utils";
 
 /** A map from GraphQL object type name -> its field names that have already been scaffolded. */
 export type History = Record<string, string[]>;
 
-const jsonFormatter = createFromBuffer(getBuffer());
+const jsonFormatter = createFromBuffer(readFileSync(getPath()));
 const configPath = ".history.json";
 
 export async function loadHistory(fs: Fs): Promise<History> {
@@ -15,6 +16,6 @@ export async function loadHistory(fs: Fs): Promise<History> {
 
 export async function writeHistory(fs: Fs, history: History): Promise<void> {
   const input = JSON.stringify(sortKeys(history));
-  const content = jsonFormatter.formatText("test.json", input);
+  const content = jsonFormatter.formatText({ filePath: "test.json", fileText: input });
   await fs.save(configPath, content);
 }
